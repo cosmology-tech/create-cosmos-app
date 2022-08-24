47031592a797cc4b4822a667b61de2698ca187e7
Dan Lynch <pyramation@gmail.com>
2022-08-24 13:50:43 -0700

    updates

--- packages/create-cosmos-app/src/cli.ts
+++ packages/create-cosmos-app/src/cli.ts
@@ -1,23 +1,5 @@
-import { prompt } from './prompt';
-import { Templates as templates } from './cmds';
-
+import { createApp } from "./git-cca-template";
+const createCosmosApp = createApp('https://github.com/cosmology-tech/create-cosmos-app.git');
 export const cli = async (argv) => {
-  const { cmd } = await prompt(
-    [
-      {
-        _: true,
-        type: 'fuzzy',
-        name: 'cmd',
-        message: 'what do you want to create?',
-        choices: Object.keys(templates)
-      }
-    ],
-    argv
-  );
-  if (typeof templates[cmd] === 'function') {
-    console.log(`calling ${cmd}`)
-    await templates[cmd](argv);
-  } else {
-    console.log('command not found.');
-  }
+  await createCosmosApp(argv);
 };
