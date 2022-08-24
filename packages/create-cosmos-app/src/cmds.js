0dc7cab07f84bd805d46cea4c61f99f120a297c9
Dan Lynch <pyramation@gmail.com>
2022-08-23 19:28:32 -0700

    cmd

--- packages/create-cosmos-app/src/cmds.js
+++ packages/create-cosmos-app/src/cmds.js
@@ -1,7 +1,10 @@
 
+  import _templates_cca_template from './templates/cca-template';
 import _templates_typescript_npm_module from './templates/typescript-npm-module';
-const Templates = {};
+  const Templates = {};
+  Templates['cca-template'] = _templates_cca_template;
 Templates['typescript-npm-module'] = _templates_typescript_npm_module;
-
-export { Templates };
-
+  
+    export { Templates }; 
+  
+    
