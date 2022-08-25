596100fac52b3913df1906de8c138f3ba539088f
Dan Lynch <pyramation@gmail.com>
2022-08-24 20:41:54 -0700

    lint

---
+++ examples/telescope-with-contracts/codegen/google/bundle.ts
@@ -0,0 +1,17 @@
+import * as _0 from "./protobuf/any";
+import * as _1 from "./protobuf/duration";
+import * as _2 from "./protobuf/timestamp";
+import * as _3 from "./protobuf/empty";
+import * as _4 from "./protobuf/struct";
+import * as _5 from "./protobuf/wrappers";
+import * as _6 from "./protobuf/field_mask";
+export namespace google {
+  export const protobuf = { ..._0,
+    ..._1,
+    ..._2,
+    ..._3,
+    ..._4,
+    ..._5,
+    ..._6
+  };
+}
--- templates/connect-multi-chain/pages/index.tsx
+++ templates/connect-multi-chain/pages/index.tsx
@@ -27,11 +27,10 @@ import {
 } from '../components';
 import styles from '../styles/Home.module.css';
 import { useWalletManager, useWallet } from '@cosmos-kit/react';
-import { mapStatusFromCosmosWallet } from "../utils";
+import { mapStatusFromCosmosWallet } from '../utils';
 import { MouseEventHandler, useState } from 'react';
 import Head from 'next/head';
 import { chainInfos, dependencies, products } from '../config';
-
 
 export default function Home() {
   const [chainId, setChainId] = useState<string | undefined>();
@@ -49,20 +48,25 @@ export default function Home() {
     disconnect();
   };
 
+  const userInfoCard = name ? (
+    <ConnectedUserInfo name={name} icon={<Astronaut />} />
+  ) : (
+    <></>
+  );
 
-  const userInfoCard = (name)
-    ? (
-      <ConnectedUserInfo name={name} icon={<Astronaut />} />
-    )
-    : <></>;
-
-  const addressCard = (chainId && address && name)
-    ? (
-      <Box maxW={{ base: "100%", md: "84%" }}>
-        <ConnectedShowAddress address={address} username={name} showLink={true} isLoading={false} />
+  const addressCard =
+    chainId && address && name ? (
+      <Box maxW={{ base: '100%', md: '84%' }}>
+        <ConnectedShowAddress
+          address={address}
+          username={name}
+          showLink={true}
+          isLoading={false}
+        />
       </Box>
-    )
-    : <></>;
+    ) : (
+      <></>
+    );
 
   const connectWalletButton = (
     <WalletConnectComponent
@@ -82,7 +86,7 @@ export default function Home() {
       }
       notExist={<NotExist buttonText="Not Exist" />}
     />
-  )
+  );
 
   const onChainChange: handleSelectChainDropdown = (
     selectedValue: ChainOption | null
@@ -109,21 +113,27 @@ export default function Home() {
       </Head>
       <main>
         <Box as={Container} maxW="5xl" mt={14} p={4}>
-          <Stack spacing={10} as={Container} maxW={'5xl'}
-            textAlign={'center'} alignContent={'center'} alignItems='center'>
+          <Stack
+            spacing={10}
+            as={Container}
+            maxW={'5xl'}
+            textAlign={'center'}
+            alignContent={'center'}
+            alignItems="center"
+          >
             <Heading
               fontWeight={800}
               fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-              lineHeight={'110%'}>
+              lineHeight={'110%'}
+            >
               Cosmos Apps Made Easy <br />
             </Heading>
             <Heading
               fontWeight={700}
               fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}
-              lineHeight={'110%'}>
-              <Text as={'span'}>
-                Welcome to {' '}
-              </Text>
+              lineHeight={'110%'}
+            >
+              <Text as={'span'}>Welcome to </Text>
               <Text as={'span'} color={'purple.500'}>
                 CosmosKit + Telescope + Next.js
               </Text>
@@ -139,20 +149,18 @@ export default function Home() {
               {chooseChain}
               {connectWalletButton}
             </Stack>
-            {
-              (chainId && !address)
-                ? <></>
-                : (
-                  <Stack
-                    spacing={{ base: 4, sm: 6 }}
-                    direction={{ base: 'column', sm: 'row' }}
-                    alignItems="center"
-                    justifyContent="center"
-                  >
-                    {(chainId && address) ? addressCard : userInfoCard}
-                  </Stack>
-                )
-            }
+            {chainId && !address ? (
+              <></>
+            ) : (
+              <Stack
+                spacing={{ base: 4, sm: 6 }}
+                direction={{ base: 'column', sm: 'row' }}
+                alignItems="center"
+                justifyContent="center"
+              >
+                {chainId && address ? addressCard : userInfoCard}
+              </Stack>
+            )}
           </Stack>
           <Grid
             mt={90}
@@ -160,14 +168,19 @@ export default function Home() {
             templateColumns={{
               base: 'repeat(1, 1fr)',
               sm: 'repeat(2, 1fr)',
-              md: 'repeat(3, 1fr)',
+              md: 'repeat(3, 1fr)'
             }}
-            gap={{ base: '3', sm: '6', md: '8' }}>
-            {products.map(product => <Product {...product} key={product.title}></Product>)}
+            gap={{ base: '3', sm: '6', md: '8' }}
+          >
+            {products.map((product) => (
+              <Product {...product} key={product.title}></Product>
+            ))}
           </Grid>
           <Divider mt={12} mb={12} />
           <SimpleGrid columns={{ base: 1, md: 2, lg: 2 }} spacing={20} mb={50}>
-            {dependencies.map((dependency) => <Dependency {...dependency}></Dependency>)}
+            {dependencies.map((dependency, i) => (
+              <Dependency key={i} {...dependency}></Dependency>
+            ))}
           </SimpleGrid>
         </Box>
       </main>
