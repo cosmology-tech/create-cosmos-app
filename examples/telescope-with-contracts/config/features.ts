f95dd85bc0e74f51846ce5c57faebb6ab9b8171c
Dan Lynch <pyramation@gmail.com>
2022-08-24 22:24:06 -0700

    chakra

--- examples/telescope-with-contracts/config/features.ts
+++ examples/telescope-with-contracts/config/features.ts
@@ -37,7 +37,7 @@ export const dependencies: FeatureProps[] = [
     {
         title: 'Chakra UI',
         text: 'A simple, modular and accessible React Component Library.',
-        href: 'https://chakra-ui.com/'
+        href: 'https://chakra-ui.com/docs/components/'
     },
     {
         title: 'Next.js',
