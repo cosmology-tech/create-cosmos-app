6d82fea0f3721283cab7d69e9bd02fc91901df87
Dan Lynch <pyramation@gmail.com>
2022-08-24 20:41:54 -0700

    lint

--- templates/connect-multi-chain/pages/_app.tsx
+++ templates/connect-multi-chain/pages/_app.tsx
@@ -1,15 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { GasPrice } from '@cosmjs/stargate'
+import { GasPrice } from '@cosmjs/stargate';
 import { ChakraProvider } from '@chakra-ui/react';
 import { defaultTheme } from '../config';
 import { ChainInfoID } from '@cosmos-kit/types';
 import { WalletManagerProvider } from '@cosmos-kit/react';
 
-const LOCAL_STORAGE_KEY = 'connectedWalletId'
+const LOCAL_STORAGE_KEY = 'connectedWalletId';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
     <WalletManagerProvider
       walletConnectClientMeta={{
@@ -17,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         description: 'A dApp using the @cosmos-kit/react library.',
         url: 'https://github.com/cosmology-tech/cosmos-wallet/tree/main/packages/react',
         // @cosmology-tech's GitHub avatar
-        icons: ['https://avatars.githubusercontent.com/u/101243801?s=200&v=4'],
+        icons: ['https://avatars.githubusercontent.com/u/101243801?s=200&v=4']
       }}
       renderLoader={() => <p>Loading...</p>}
       localStorageKey={LOCAL_STORAGE_KEY}
@@ -25,26 +24,19 @@ function MyApp({ Component, pageProps }: AppProps) {
       getSigningCosmWasmClientOptions={(chainInfo) => ({
         gasPrice: GasPrice.fromString(
           '0.0025' + chainInfo.feeCurrencies[0].coinMinimalDenom
-        ),
+        )
       })}
       getSigningStargateClientOptions={(chainInfo) => ({
         gasPrice: GasPrice.fromString(
           '0.0025' + chainInfo.feeCurrencies[0].coinMinimalDenom
-        ),
+        )
       })}
-    // Choose a different RPC node for the desired chain.
-    // chainInfoOverrides={[
-    //   {
-    //     ...ChainInfoMap[ChainInfoID.Juno1],
-    //     rpc: "https://another.rpc.com",
-    //   }
-    // ]}
     >
       <ChakraProvider theme={defaultTheme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </WalletManagerProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
