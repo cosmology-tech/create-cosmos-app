02fd5474d3d4e964ba3fddb6ffa93982e80884b5
JunLiu93 <853480121@qq.com>
2022-08-29 12:47:32 +0800

    delete extra codes

--- templates/connect-chain/components/index.tsx
+++ templates/connect-chain/components/index.tsx
@@ -2,8 +2,6 @@ export * from "./types";
 export * from "./wallet-connect";
 export * from "./user-card";
 export * from "./astronaut";
-export * from "./choose-chain";
-export * from "./chain-dropdown";
 export * from "./address-card";
 export * from "./features";
 export * from "./warn-block";
