5a3f75c0837483a9a024568e8d1de85ca652a21e
Dan Lynch <pyramation@gmail.com>
2022-08-30 09:25:16 -0700

    cosmoskit

--- templates/connect-chain/pages/index.tsx
+++ templates/connect-chain/pages/index.tsx
@@ -1,5 +1,5 @@
-import { MouseEventHandler } from "react";
-import Head from "next/head";
+import { MouseEventHandler } from 'react';
+import Head from 'next/head';
 import {
   Box,
   Divider,
@@ -15,11 +15,11 @@ import {
   useColorMode,
   useColorModeValue,
   GridItem,
-  Center,
-} from "@chakra-ui/react";
-import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
-import { FiAlertTriangle } from "react-icons/fi";
-import { useWalletManager, useWallet } from "@cosmos-kit/react";
+  Center
+} from '@chakra-ui/react';
+import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
+import { FiAlertTriangle } from 'react-icons/fi';
+import { useWalletManager, useWallet } from '@cosmos-kit/react';
 import {
   Connected,
   ConnectedUserInfo,
@@ -34,10 +34,10 @@ import {
   Dependency,
   ConnectStatusWarn,
   RejectedWarn,
-  CopyAddressBtn,
-} from "../components";
-import { mapStatusFromCosmosWallet } from "../utils";
-import { dependencies, products } from "../config";
+  CopyAddressBtn
+} from '../components';
+import { mapStatusFromCosmosWallet } from '../utils';
+import { dependencies, products } from '../config';
 
 export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -97,14 +97,14 @@ export default function Home() {
       <Flex justifyContent="end" mb={4}>
         <Button variant="outline" px={0} onClick={toggleColorMode}>
           <Icon
-            as={colorMode === "light" ? BsFillMoonStarsFill : BsFillSunFill}
+            as={colorMode === 'light' ? BsFillMoonStarsFill : BsFillSunFill}
           />
         </Button>
       </Flex>
       <Box textAlign="center">
         <Heading
           as="h1"
-          fontSize={{ base: "3xl", sm: "4xl", md: "5xl" }}
+          fontSize={{ base: '3xl', sm: '4xl', md: '5xl' }}
           fontWeight="extrabold"
           mb={3}
         >
@@ -113,14 +113,14 @@ export default function Home() {
         <Heading
           as="h1"
           fontWeight="bold"
-          fontSize={{ base: "2xl", sm: "3xl", md: "4xl" }}
+          fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}
         >
           <Text as="span">Welcome to&nbsp;</Text>
           <Text
             as="span"
-            color={useColorModeValue("primary.500", "primary.200")}
+            color={useColorModeValue('primary.500', 'primary.200')}
           >
-            CosmosKit.js + Telescope + Next.js
+            CosmosKit + Telescope + Next.js
           </Text>
         </Heading>
       </Box>
@@ -139,10 +139,10 @@ export default function Home() {
               justifyContent="center"
               alignItems="center"
               borderRadius="lg"
-              bg={useColorModeValue("white", "blackAlpha.400")}
+              bg={useColorModeValue('white', 'blackAlpha.400')}
               boxShadow={useColorModeValue(
-                "0 0 2px #dfdfdf, 0 0 6px -2px #d3d3d3",
-                "0 0 2px #363636, 0 0 8px -2px #4f4f4f"
+                '0 0 2px #dfdfdf, 0 0 6px -2px #d3d3d3',
+                '0 0 2px #363636, 0 0 8px -2px #4f4f4f'
               )}
               spacing={4}
               px={4}
@@ -159,8 +159,8 @@ export default function Home() {
       </Center>
       <Grid
         templateColumns={{
-          md: "repeat(2, 1fr)",
-          lg: "repeat(3, 1fr)",
+          md: 'repeat(2, 1fr)',
+          lg: 'repeat(3, 1fr)'
         }}
         gap={8}
         mb={14}
@@ -169,7 +169,7 @@ export default function Home() {
           <Product key={product.title} {...product}></Product>
         ))}
       </Grid>
-      <Grid templateColumns={{ md: "1fr 1fr" }} gap={8} mb={20}>
+      <Grid templateColumns={{ md: '1fr 1fr' }} gap={8} mb={20}>
         {dependencies.map((dependency) => (
           <Dependency key={dependency.title} {...dependency}></Dependency>
         ))}
