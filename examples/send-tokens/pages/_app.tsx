fd20d12f1df2b16beda48aaf9a7f1deab0d91996
Dan Lynch <pyramation@gmail.com>
2022-09-24 14:51:32 -0500

    send tokens example

---
+++ examples/send-tokens/pages/_app.tsx
@@ -0,0 +1,33 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WalletProvider } from '@cosmos-kit/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { defaultTheme } from '../config';
+import { wallets } from '@cosmos-kit/config';
+import { chains } from 'chain-registry';
+import { getSigningCosmosClientOptions } from 'interchain';
+
+import { SignerOptions } from '@cosmos-kit/core';
+import { Chain } from '@chain-registry/types';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const signerOptions: SignerOptions = {
+    stargate: (_chain: Chain) => {
+      return getSigningCosmosClientOptions();
+    }
+  };
+
+  return (
+    <ChakraProvider theme={defaultTheme}>
+      <WalletProvider
+        chains={chains}
+        wallets={wallets}
+        signerOptions={signerOptions}
+      >
+        <Component {...pageProps} />
+      </WalletProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
