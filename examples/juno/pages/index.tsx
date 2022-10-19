ff87ee9c684ad939dc6378fadbc03104ab9113c0
yujulchen <74940804+yujulchen@users.noreply.github.com>
2022-10-19 13:24:31 +0800

    format send tokens style

--- examples/juno/pages/index.tsx
+++ examples/juno/pages/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import { useWallet } from '@cosmos-kit/react';
-import { StdFee } from '@cosmjs/amino';
-import { SigningStargateClient } from '@cosmjs/stargate';
-import BigNumber from 'bignumber.js';
+import { useState } from "react";
+import Head from "next/head";
+import { useWallet } from "@cosmos-kit/react";
+import { StdFee } from "@cosmjs/amino";
+import { SigningStargateClient } from "@cosmjs/stargate";
+import BigNumber from "bignumber.js";
 
 import {
   Box,
@@ -17,20 +18,32 @@ import {
   Flex,
   Icon,
   useColorMode,
-  useColorModeValue
-} from '@chakra-ui/react';
-import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
-import { chainassets, chainName, coin, dependencies, products } from '../config';
+  useColorModeValue,
+  Center,
+} from "@chakra-ui/react";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+import {
+  chainassets,
+  chainName,
+  coin,
+  dependencies,
+  products,
+} from "../config";
 
-import { WalletStatus } from '@cosmos-kit/core';
-import { Product, Dependency, WalletSection } from '../components';
-import { cosmos } from 'juno-network';
-import Head from 'next/head';
+import { WalletStatus } from "@cosmos-kit/core";
+import {
+  Product,
+  Dependency,
+  WalletSection,
+  handleChangeColorModeValue,
+} from "../components";
+import { cosmos } from "juno-network";
+import { SendTokensCard } from "../components/react/send-tokens-card";
 
 const library = {
-  title: 'Juno Network',
-  text: 'Typescript libraries for the Juno ecosystem',
-  href: 'https://github.com/CosmosContracts/typescript'
+  title: "Juno Network",
+  text: "Typescript libraries for the Juno ecosystem",
+  href: "https://github.com/CosmosContracts/typescript",
 };
 
 const sendTokens = (
@@ -41,7 +54,7 @@ const sendTokens = (
   return async () => {
     const stargateClient = await getStargateClient();
     if (!stargateClient || !address) {
-      console.error('stargateClient undefined or address undefined.');
+      console.error("stargateClient undefined or address undefined.");
       return;
     }
 
@@ -51,21 +64,21 @@ const sendTokens = (
       amount: [
         {
           denom: coin.base,
-          amount: '1000'
-        }
+          amount: "1000",
+        },
       ],
       toAddress: address,
-      fromAddress: address
+      fromAddress: address,
     });
 
     const fee: StdFee = {
       amount: [
         {
           denom: coin.base,
-          amount: '2000'
-        }
+          amount: "2000",
+        },
       ],
-      gas: '86364'
+      gas: "86364",
     };
     const response = await stargateClient.signAndBroadcast(address, [msg], fee);
     setResp(JSON.stringify(response, null, 2));
@@ -75,15 +88,11 @@ const sendTokens = (
 export default function Home() {
   const { colorMode, toggleColorMode } = useColorMode();
 
-  const {
-    getStargateClient,
-    address,
-    currentWallet,
-    walletStatus
-  } = useWallet();
+  const { getStargateClient, address, currentWallet, walletStatus } =
+    useWallet();
 
   const [balance, setBalance] = useState(new BigNumber(0));
-  const [resp, setResp] = useState('');
+  const [resp, setResp] = useState("");
   const getBalance = async () => {
     if (!address) {
       setBalance(new BigNumber(0));
@@ -93,19 +102,19 @@ export default function Home() {
     let rpcEndpoint = await currentWallet?.getRpcEndpoint();
 
     if (!rpcEndpoint) {
-      console.log('no rpc endpoint — using a fallback');
+      console.log("no rpc endpoint — using a fallback");
       rpcEndpoint = `https://rpc.cosmos.directory/${chainName}`;
     }
 
     // get RPC client
     const client = await cosmos.ClientFactory.createRPCQueryClient({
-      rpcEndpoint
+      rpcEndpoint,
     });
 
     // fetch balance
     const balance = await client.cosmos.bank.v1beta1.balance({
       address,
-      denom: chainassets?.assets[0].base as string
+      denom: chainassets?.assets[0].base as string,
     });
 
     // Get the display exponent
@@ -119,7 +128,8 @@ export default function Home() {
     setBalance(amount);
   };
 
-  const color = useColorModeValue('primary.500', 'primary.200');
+  const color = useColorModeValue("primary.500", "primary.200");
+
   return (
     <Container maxW="5xl" py={10}>
       <Head>
@@ -130,14 +140,18 @@ export default function Home() {
       <Flex justifyContent="end" mb={4}>
         <Button variant="outline" px={0} onClick={toggleColorMode}>
           <Icon
-            as={colorMode === 'light' ? BsFillMoonStarsFill : BsFillSunFill}
+            as={handleChangeColorModeValue(
+              colorMode,
+              BsFillMoonStarsFill,
+              BsFillSunFill
+            )}
           />
         </Button>
       </Flex>
       <Box textAlign="center">
         <Heading
           as="h1"
-          fontSize={{ base: '3xl', sm: '4xl', md: '5xl' }}
+          fontSize={{ base: "3xl", md: "5xl" }}
           fontWeight="extrabold"
           mb={3}
         >
@@ -146,88 +160,57 @@ export default function Home() {
         <Heading
           as="h1"
           fontWeight="bold"
-          fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}
+          fontSize={{ base: "2xl", md: "4xl" }}
         >
           <Text as="span">Welcome to&nbsp;</Text>
           <Text as="span" color={color}>
-            CosmosKit + Next.js +{' '}
-            <a href={library.href} target="_blank" rel="noreferrer">
+            CosmosKit&nbsp;+&nbsp;Next.js&nbsp;+&nbsp;
+            <Link href={library.href} target="_blank" rel="noreferrer">
               {library.title}
-            </a>
+            </Link>
           </Text>
         </Heading>
       </Box>
+
       <WalletSection />
 
-      {walletStatus === WalletStatus.Disconnected && (
-        <Box textAlign="center">
-          <Heading
-            as="h3"
-            fontSize={{ base: '1xl', sm: '2xl', md: '2xl' }}
-            fontWeight="extrabold"
-            m={30}
-          >
-            Connect your wallet!
-          </Heading>
-        </Box>
-      )}
+      <Center mb={16}>
+        <SendTokensCard
+          isConnectWallet={walletStatus === WalletStatus.Connected}
+          balance={balance.toNumber()}
+          response={resp}
+          sendTokensButtonText="Send Tokens"
+          handleClickSendTokens={sendTokens(
+            getStargateClient as () => Promise<SigningStargateClient>,
+            setResp as () => any,
+            address as string
+          )}
+          handleClickGetBalance={getBalance}
+        />
+      </Center>
 
-      {walletStatus === WalletStatus.Connected && (
-        <Box textAlign="center">
-          <Flex mb={4}>
-            <Button
-              variant="outline"
-              onClick={sendTokens(
-                getStargateClient as () => Promise<SigningStargateClient>,
-                setResp as () => any,
-                address as string
-              )}
-            >
-              Send Tokens (to self)
-            </Button>
-            <Text as="span">Balance&nbsp;</Text>
-            <Text as="span" color={color}>
-              Balance: {balance.toNumber()}
-            </Text>
-
-            <Button variant="outline" onClick={getBalance}>
-              Fetch Balance
-            </Button>
-          </Flex>
-        </Box>
-      )}
-
-      {!!resp && (
-        <>
-          <Container>Response: </Container>
-          <pre>{resp}</pre>
-        </>
-      )}
-
-      <Dependency key={library.title} {...library}></Dependency>
-
-      <Box mb={3}>
+      <Box mb={16}>
         <Divider />
       </Box>
-
       <Grid
         templateColumns={{
-          md: 'repeat(2, 1fr)',
-          lg: 'repeat(3, 1fr)'
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
         }}
         gap={8}
         mb={14}
       >
         {products.map((product) => (
-          <Product key={product.title} {...product}></Product>
+          <Product key={product.title} {...product} />
+        ))}
+      </Grid>
+      <Grid templateColumns={{ md: "repeat(3, 1fr)" }} gap={8} mb={20}>
+        <Dependency {...library} />
+        {dependencies.map((dependency) => (
+          <Dependency key={dependency.title} {...dependency} />
         ))}
       </Grid>
 
-      <Grid templateColumns={{ md: '1fr 1fr' }} gap={8} mb={20}>
-        {dependencies.map((dependency) => (
-          <Dependency key={dependency.title} {...dependency}></Dependency>
-        ))}
-      </Grid>
       <Box mb={3}>
         <Divider />
       </Box>
