eef92e3f3e37eb32a0ce1ea4987acf5fd391945d
Dan Lynch <pyramation@gmail.com>
2022-10-23 22:04:44 -0700

    defaults

--- examples/osmosis-cosmwasm/components/react/hackcw20.tsx
+++ examples/osmosis-cosmwasm/components/react/hackcw20.tsx
@@ -3,12 +3,13 @@ import { Heading } from "@chakra-ui/react";
 import { WalletStatus } from "@cosmos-kit/core";
 import { useWallet } from "@cosmos-kit/react";
 import { useHackCw20Balance } from "../../hooks/use-hack-cw20-balance";
+import { cw20ContractAddress } from "../../config";
 
 export default function HackCw20() {
   const { walletStatus } = useWallet();
 
   const { balance } = useHackCw20Balance(
-    "osmo1y0ywcujptlmnx4fgstlqfp7nftc8w5qndsfds9wxwtm0ltjpzp4qdj09j8"
+    cw20ContractAddress
   );
 
   return (
