78453e5acd5d20ca944410e2b41abf469848394a
Dan Lynch <pyramation@gmail.com>
2022-10-24 03:00:42 -0700

    wasm

--- examples/osmosis-cosmwasm/config/defaults.ts
+++ examples/osmosis-cosmwasm/config/defaults.ts
@@ -10,6 +10,7 @@ export const feeDenom = 'uosmo';
 // export const stakingDenom = 'umlg';
 // export const feeDenom = 'uand';
 
+// export const cw20ContractAddress = 'wasm1p7vmrhl3s0fyl0m9hk2hlm7uuxq84hztur63n8ryh85chh30vt6q89shcv'
 export const cw20ContractAddress = 'osmo1y0ywcujptlmnx4fgstlqfp7nftc8w5qndsfds9wxwtm0ltjpzp4qdj09j8'
 
 export const chainassets: AssetList = assets.find(
