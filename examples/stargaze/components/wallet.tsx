94b7e258efe2138fd81d11bdc68a12877aafe3cc
liujun93 <ljun8575@gmail.com>
2022-11-09 17:58:31 +0800

    modify ui

--- examples/stargaze/components/wallet.tsx
+++ examples/stargaze/components/wallet.tsx
@@ -105,7 +105,7 @@ export const WalletSection = () => {
     />
   );
 
-  const userInfo = (
+  const userInfo = username && (
     <ConnectedUserInfo username={username} icon={<Astronaut />} />
   );
   const addressBtn = currentChainName && (
