c9639c9de3a5984eea293079e55a787b72b1f1ac
adairrr <32375605+adairrr@users.noreply.github.com>
2022-11-18 13:31:26 -0800

    Update react-query example with hook factory

--- examples/telescope/codegen/cosmos/bank/v1beta1/query.rpc.react-query.ts
+++ examples/telescope/codegen/cosmos/bank/v1beta1/query.rpc.react-query.ts
@@ -1,33 +1,63 @@
-import { Rpc } from "../../../helpers";
-import * as _m0 from "protobufjs/minimal";
-import { QueryClient, createProtobufRpcClient } from "@cosmjs/stargate";
-import { QueryBalanceRequest, QueryBalanceResponse, QueryAllBalancesRequest, QueryAllBalancesResponse, QuerySpendableBalancesRequest, QuerySpendableBalancesResponse, QueryTotalSupplyRequest, QueryTotalSupplyResponse, QuerySupplyOfRequest, QuerySupplyOfResponse, QueryParamsRequest, QueryParamsResponse, QueryDenomMetadataRequest, QueryDenomMetadataResponse, QueryDenomsMetadataRequest, QueryDenomsMetadataResponse, QueryDenomOwnersRequest, QueryDenomOwnersResponse } from "./query";
-import { QueryClientImpl } from "./query.rpc.Query";
+import { Rpc } from '../../../helpers'
+import * as _m0 from 'protobufjs/minimal'
+import { QueryClient, createProtobufRpcClient } from '@cosmjs/stargate'
+import {
+  QueryBalanceRequest,
+  QueryBalanceResponse,
+  QueryAllBalancesRequest,
+  QueryAllBalancesResponse,
+  QuerySpendableBalancesRequest,
+  QuerySpendableBalancesResponse,
+  QueryTotalSupplyRequest,
+  QueryTotalSupplyResponse,
+  QuerySupplyOfRequest,
+  QuerySupplyOfResponse,
+  QueryParamsRequest,
+  QueryParamsResponse,
+  QueryDenomMetadataRequest,
+  QueryDenomMetadataResponse,
+  QueryDenomsMetadataRequest,
+  QueryDenomsMetadataResponse,
+  QueryDenomOwnersRequest,
+  QueryDenomOwnersResponse,
+} from './query'
+import { QueryClientImpl } from './query.rpc.Query'
+
+import { Tendermint34Client, HttpEndpoint } from "@cosmjs/tendermint-rpc";
 
 import {
-    useQuery,
-    UseQueryOptions,
-} from '@tanstack/react-query';
+  useQuery,
+  UseQueryOptions,
+} from '@tanstack/react-query'
 
 interface ReactQueryParams<TResponse, TData = TResponse> {
-    options?: UseQueryOptions<TResponse, Error, TData>;
+  options?: UseQueryOptions<TResponse, Error, TData>;
 }
 
 export interface UseBalanceQuery<TData> extends ReactQueryParams<QueryBalanceResponse, TData> {
-    request: QueryBalanceRequest;
+  request: QueryBalanceRequest;
 }
 
-export const createRpcQueryHooks = (base: QueryClient) => {
-    const rpc = createProtobufRpcClient(base);
-    const queryService = new QueryClientImpl(rpc);
-    return {
-        useBalance<TData = QueryBalanceResponse>({
-            request,
-            options
-        }: UseBalanceQuery<TData>) {
-            return useQuery<QueryBalanceResponse, Error, TData>(["queryBalance", request], () => {
-                return queryService.balance(request)
-            }, options);
-        }
-    };
-};
+export const createRpcQueryHooks = (rpcEndpoint: string | HttpEndpoint) => {
+  const useBalance = <TData = QueryBalanceResponse>({
+        request,
+        options,
+      }: UseBalanceQuery<TData>) =>
+    useQuery<QueryBalanceResponse, Error, TData>(['queryBalance', request], async () => {
+      const tmClient = await Tendermint34Client.connect(rpcEndpoint)
+      //@ts-ignore
+      const client = new QueryClient(tmClient)
+
+      const rpc = createProtobufRpcClient(client)
+      const queryService = new QueryClientImpl(rpc)
+
+      return queryService.balance(request)
+    }, options)
+
+
+  return {
+    useBalance
+  }
+}
+
+export default createRpcQueryHooks
