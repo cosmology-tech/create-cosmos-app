61f31752c835b13fb3fdca056ce5dbe4b8f2351a
Dan Lynch <pyramation@gmail.com>
2022-12-14 22:07:32 -0800

    osmosis settings

--- examples/osmosis-cosmwasm/pages/_app.tsx
+++ examples/osmosis-cosmwasm/pages/_app.tsx
@@ -2,22 +2,23 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { WalletProvider } from '@cosmos-kit/react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { chainName, defaultTheme } from '../config';
+import { defaultTheme } from '../config';
 import { wallets as keplrWallets } from '@cosmos-kit/keplr';
 import { wallets as cosmostationWallets } from '@cosmos-kit/cosmostation';
 import { wallets as leapWallets } from '@cosmos-kit/leap';
-
+import { aminoTypes, registry } from '../config/defaults';
 import { assets, chains } from 'chain-registry';
-import { getSigningCosmosClientOptions } from 'osmojs';
 import { GasPrice } from '@cosmjs/stargate';
-
 import { SignerOptions } from '@cosmos-kit/core';
 import { Chain } from '@chain-registry/types';
 
 function CreateCosmosApp({ Component, pageProps }: AppProps) {
   const signerOptions: SignerOptions = {
     signingStargate: (_chain: Chain) => {
-      return getSigningCosmosClientOptions();
+      return {
+        aminoTypes,
+        registry
+      }
     },
     signingCosmwasm: (chain: Chain) => {
       switch (chain.chain_name) {
