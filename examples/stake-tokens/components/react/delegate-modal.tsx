48a928c42e944ca1d21944d1efc1ad973c7df72b
luca <uniqueye608@gmail.com>
2023-01-05 20:20:51 +0800

    add unbonding days for all validators

--- examples/stake-tokens/components/react/delegate-modal.tsx
+++ examples/stake-tokens/components/react/delegate-modal.tsx
@@ -57,63 +57,71 @@ export const DelegateWarning = ({
   unbondingDays,
 }: {
   unbondingDays: number;
-}) => (
-  <Alert
-    status="error"
-    borderRadius={10}
-    flexDir="column"
-    alignItems="flex-start"
-    bgColor="red.50"
-    mt={4}
-  >
-    <Flex>
-      <AlertIcon />
-      <AlertTitle color="red.500">
-        Staking will lock your funds for {unbondingDays} days
-      </AlertTitle>
-    </Flex>
-    <AlertDescription fontSize="sm" color="red.400" lineHeight="short" mt={2}>
-      You will need to undelegate in order for your staked assets to be liquid
-      again. This process will take {unbondingDays} days to complete.
-    </AlertDescription>
-  </Alert>
-);
+}) => {
+  if (!unbondingDays) return <></>;
+
+  return (
+    <Alert
+      status="error"
+      borderRadius={10}
+      flexDir="column"
+      alignItems="flex-start"
+      bgColor="red.50"
+      mt={4}
+    >
+      <Flex>
+        <AlertIcon />
+        <AlertTitle color="red.500">
+          Staking will lock your funds for {unbondingDays} days
+        </AlertTitle>
+      </Flex>
+      <AlertDescription fontSize="sm" color="red.400" lineHeight="short" mt={2}>
+        You will need to undelegate in order for your staked assets to be liquid
+        again. This process will take {unbondingDays} days to complete.
+      </AlertDescription>
+    </Alert>
+  );
+};
 
 export const UndelegateWarning = ({
   unbondingDays,
 }: {
   unbondingDays: number;
-}) => (
-  <Alert
-    status="error"
-    borderRadius={10}
-    flexDir="column"
-    alignItems="flex-start"
-    bgColor="red.50"
-  >
-    <Flex>
-      <AlertIcon />
-      <AlertTitle color="red.500">
-        Once the unbonding period begins you will:
-      </AlertTitle>
-    </Flex>
-    <AlertDescription
-      fontSize="sm"
-      color="red.400"
-      lineHeight="short"
-      mt={2}
-      ml={1}
+}) => {
+  if (!unbondingDays) return <></>;
+
+  return (
+    <Alert
+      status="error"
+      borderRadius={10}
+      flexDir="column"
+      alignItems="flex-start"
+      bgColor="red.50"
     >
-      <UnorderedList>
-        <ListItem>not receive staking rewards</ListItem>
-        <ListItem>not be able to cancel the unbonding</ListItem>
-        <ListItem>
-          need to wait {unbondingDays} days for the amount to be liquid
-        </ListItem>
-      </UnorderedList>
-    </AlertDescription>
-  </Alert>
-);
+      <Flex>
+        <AlertIcon />
+        <AlertTitle color="red.500">
+          Once the unbonding period begins you will:
+        </AlertTitle>
+      </Flex>
+      <AlertDescription
+        fontSize="sm"
+        color="red.400"
+        lineHeight="short"
+        mt={2}
+        ml={1}
+      >
+        <UnorderedList>
+          <ListItem>not receive staking rewards</ListItem>
+          <ListItem>not be able to cancel the unbonding</ListItem>
+          <ListItem>
+            need to wait {unbondingDays} days for the amount to be liquid
+          </ListItem>
+        </UnorderedList>
+      </AlertDescription>
+    </Alert>
+  );
+};
 
 export const StatBox = ({
   label,
