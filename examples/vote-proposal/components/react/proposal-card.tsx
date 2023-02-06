fac06aa68b752dc73258ea13cf8461d18dce03b5
luca <uniqueye608@gmail.com>
2023-02-06 23:20:27 +0800

    finish voting example

--- examples/vote-proposal/components/react/proposal-card.tsx
+++ examples/vote-proposal/components/react/proposal-card.tsx
@@ -1,4 +1,9 @@
-import { CheckIcon, ChevronRightIcon } from '@chakra-ui/icons';
+import React, { useMemo } from 'react';
+import type { Proposal } from 'interchain/types/codegen/cosmos/gov/v1beta1/gov';
+import dayjs from 'dayjs';
+import { cosmos } from 'interchain';
+import { VoteOption } from '../types';
+import { decodeUint8Arr, Votes } from './vote';
 import {
   Badge,
   Box,
@@ -6,148 +11,198 @@ import {
   Flex,
   Grid,
   GridItem,
-  Icon,
-  Progress,
   Spacer,
-  Stack,
   Text,
+  useColorMode,
+  useColorModeValue,
 } from '@chakra-ui/react';
-import React from 'react';
-import { BsDot, BsCheck } from 'react-icons/bs';
 
-export const ProposalCard = ({ openModal }: { openModal: () => void }) => {
+const ProposalStatus = cosmos.gov.v1beta1.ProposalStatus;
+
+export const getTitleFromDecoded = (decodedStr: string) => {
+  return decodedStr.slice(0, 250).match(/[A-Z][A-Za-z].*(?=\u0012)/)?.[0];
+};
+
+export const getPercentage = (option: string | undefined, total: number) => {
+  if (!total) return '0.00%';
+  const voted = option ? Number(option) : 0;
+  return ((voted / total) * 100).toFixed(2) + '%';
+};
+
+export const VoteColor: { [key in VoteOption]: string } = {
+  [VoteOption.YES]: '#17a572',
+  [VoteOption.NO]: '#ce4256',
+  [VoteOption.NWV]: '#ff5b6d',
+  [VoteOption.ABSTAIN]: '#546198',
+};
+
+export const StatusBadge = ({ status }: { status: number }) => {
+  let statusConfig: { color: string; name: string } = {
+    color: 'purple',
+    name: 'Deposit Period',
+  };
+
+  switch (status) {
+    case ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD:
+      break;
+    case ProposalStatus.PROPOSAL_STATUS_VOTING_PERIOD:
+      statusConfig = {
+        color: 'twitter',
+        name: 'Voting Period',
+      };
+      break;
+    case ProposalStatus.PROPOSAL_STATUS_PASSED:
+      statusConfig = {
+        color: 'green',
+        name: 'Passed',
+      };
+      break;
+    case ProposalStatus.PROPOSAL_STATUS_REJECTED:
+      statusConfig = {
+        color: 'red',
+        name: 'Rejected',
+      };
+      break;
+    default:
+      break;
+  }
+
   return (
-    <Stack spacing={4}>
-      <Grid
-        h="120px"
-        py={4}
-        templateColumns="repeat(13, 1fr)"
-        // gap={4}
-        bgColor="#fbfcfe"
-        borderColor="gray.400"
-        borderRadius={10}
-        transition="all 0.2s linear"
-        _hover={{
-          backgroundColor: 'gray.100',
-          cursor: 'pointer',
-        }}
-        onClick={() => openModal()}
-      >
-        <GridItem colSpan={2}>
-          <Center w="100%" h="100%">
-            # 000029
-          </Center>
-        </GridItem>
-        <GridItem colSpan={9} py={2}>
-          <Flex flexDirection="column" h="100%">
-            <Flex gap={2} alignItems="center">
-              <Text fontSize="lg">Cosmos Option Vaults by CzarDAO</Text>
-              <Badge
-                colorScheme="purple"
-                variant="solid"
-                borderRadius={4}
-                h="min-content"
+    <Badge colorScheme={statusConfig.color} variant="subtle" borderRadius={4}>
+      <Flex alignItems="center">{statusConfig.name}</Flex>
+    </Badge>
+  );
+};
+
+const VotedBadge = () => (
+  <Badge colorScheme="purple" variant="solid" borderRadius={4} h="min-content">
+    Voted
+  </Badge>
+);
+
+export const ProposalCard = ({
+  proposal,
+  handleClick,
+  votes,
+}: {
+  proposal: Proposal;
+  handleClick: () => void;
+  votes: Votes | undefined;
+}) => {
+  const { colorMode } = useColorMode();
+
+  const totalVotes = useMemo(() => {
+    if (!proposal.finalTallyResult) return 0;
+    const total = Object.values(proposal.finalTallyResult).reduce(
+      (prev, cur) => prev + Number(cur),
+      0
+    );
+    return total ? total : 0;
+  }, [proposal]);
+
+  const isVoted = votes && votes[proposal.proposalId.low];
+
+  return (
+    <Grid
+      h="120px"
+      py={4}
+      templateColumns="repeat(13, 1fr)"
+      bgColor={useColorModeValue('#fbfcfe', 'whiteAlpha.200')}
+      borderColor="gray.400"
+      borderRadius={10}
+      transition="all 0.2s linear"
+      _hover={{
+        backgroundColor: useColorModeValue('gray.100', 'gray.700'),
+        cursor: 'pointer',
+      }}
+      onClick={handleClick}
+    >
+      <GridItem colSpan={2}>
+        <Center w="100%" h="100%">
+          # {proposal.proposalId.low.toString().padStart(6, '0')}
+        </Center>
+      </GridItem>
+      <GridItem colSpan={9} py={2}>
+        <Flex flexDirection="column" h="100%">
+          <Flex gap={2} alignItems="center">
+            <Text fontSize="lg">
+              {getTitleFromDecoded(decodeUint8Arr(proposal.content?.value))}
+            </Text>
+            {isVoted && <VotedBadge />}
+          </Flex>
+          <Spacer />
+          <Flex flexDirection="column" h="44%">
+            <Flex alignItems="center" fontSize="sm">
+              <Text color={useColorModeValue('gray.600', 'gray.400')}>
+                {proposal.status ===
+                ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD
+                  ? 'Deposit'
+                  : 'Voting'}
+                &nbsp;end time: &nbsp;
+              </Text>
+              <Text
+                color={useColorModeValue('gray.600', 'gray.400')}
+                fontWeight="semibold"
               >
-                Voted
-              </Badge>
-            </Flex>
-            <Spacer />
-            <Flex flexDirection="column" h="44%">
-              <Flex alignItems="center" fontSize="sm">
-                <Text color="gray.600">Voting end time: &nbsp;</Text>
-                <Text color="gray.600" fontWeight="semibold">
-                  2023-01-22 02:27
-                </Text>
-              </Flex>
-              <Spacer />
-              <Flex gap="1px">
-                <Box w="86%" h="3px" bgColor="green.500" />
-                <Box w="4%" h="3px" bgColor="red.500" />
-                <Box w="10%" h="3px" bgColor="gray.500" />
-              </Flex>
-            </Flex>
-          </Flex>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <Flex
-            w="100%"
-            h="100%"
-            alignItems="center"
-            px={4}
-            justifyContent="center"
-          >
-            <Badge colorScheme="purple" variant="subtle" borderRadius={4}>
-              <Flex alignItems="center">
-                {/* <Icon as={BsDot} fontSize="xl" w="16px" />  */}
-                Voting Period
-              </Flex>
-            </Badge>
-          </Flex>
-        </GridItem>
-      </Grid>
-
-      {/* ------------ */}
-
-      <Grid
-        h="120px"
-        py={4}
-        templateColumns="repeat(13, 1fr)"
-        // gap={4}
-        bgColor="#fbfcfe"
-        borderColor="gray.400"
-        borderRadius={10}
-        transition="all 0.2s linear"
-        _hover={{
-          backgroundColor: 'gray.100',
-          cursor: 'pointer',
-        }}
-      >
-        <GridItem colSpan={2}>
-          <Center w="100%" h="100%">
-            # 000029
-          </Center>
-        </GridItem>
-        <GridItem colSpan={9} py={2}>
-          <Flex flexDirection="column" h="100%">
-            <Flex gap={2} alignItems="center">
-              <Text fontSize="lg">
-                Community Spend Proposal for AmigoSound Project
+                {dayjs(
+                  proposal.status ===
+                    ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD
+                    ? proposal.depositEndTime
+                    : proposal.votingEndTime
+                ).format('YYYY-MM-DD hh:mm')}
               </Text>
             </Flex>
             <Spacer />
-            <Flex flexDirection="column" h="44%">
-              <Flex alignItems="center" fontSize="sm">
-                <Text color="gray.600">Voting end time: &nbsp;</Text>
-                <Text color="gray.600" fontWeight="semibold">
-                  2023-01-22 02:27
-                </Text>
-              </Flex>
-              <Spacer />
+            {totalVotes ? (
               <Flex gap="1px">
-                <Box w="60%" h="3px" bgColor="green.500" />
-                <Box w="12%" h="3px" bgColor="red.500" />
-                <Box w="28%" h="3px" bgColor="gray.500" />
+                <Box
+                  w={getPercentage(proposal.finalTallyResult?.yes, totalVotes)}
+                  h="3px"
+                  bgColor={VoteColor.YES}
+                />
+                <Box
+                  w={getPercentage(proposal.finalTallyResult?.no, totalVotes)}
+                  h="3px"
+                  bgColor={VoteColor.NO}
+                />
+                <Box
+                  w={getPercentage(
+                    proposal.finalTallyResult?.noWithVeto,
+                    totalVotes
+                  )}
+                  h="3px"
+                  bgColor={VoteColor.NWV}
+                />
+                <Box
+                  w={getPercentage(
+                    proposal.finalTallyResult?.abstain,
+                    totalVotes
+                  )}
+                  h="3px"
+                  bgColor={VoteColor.ABSTAIN}
+                />
               </Flex>
-            </Flex>
+            ) : (
+              <Box
+                w="100%"
+                h="3px"
+                bgColor={colorMode === 'light' ? 'gray.200' : 'gray.600'}
+              />
+            )}
           </Flex>
-        </GridItem>
-        <GridItem colSpan={2}>
-          <Flex
-            w="100%"
-            h="100%"
-            alignItems="center"
-            px={4}
-            justifyContent="center"
-          >
-            <Badge colorScheme="green" variant="subtle" borderRadius={4}>
-              <Flex>
-                {/* <Icon as={BsCheck} fontSize="lg" /> */}
-                Passed
-              </Flex>
-            </Badge>
-          </Flex>
-        </GridItem>
-      </Grid>
-    </Stack>
+        </Flex>
+      </GridItem>
+      <GridItem colSpan={2}>
+        <Flex
+          w="100%"
+          h="100%"
+          alignItems="center"
+          px={4}
+          justifyContent="center"
+        >
+          <StatusBadge status={proposal.status} />
+        </Flex>
+      </GridItem>
+    </Grid>
   );
 };
