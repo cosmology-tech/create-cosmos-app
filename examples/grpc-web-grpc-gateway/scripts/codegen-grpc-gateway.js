d2bf3feb1d7ee7ce07b73ae8dd288109c007bd71
hoangdv2429 <hoangdv2429@gmail.com>
2023-06-06 13:57:02 +0700

    add proto folder and scripts

---
+++ examples/grpc-web-grpc-gateway/scripts/codegen-grpc-gateway.js
@@ -0,0 +1,59 @@
+const { join, resolve } = require('path');
+const telescope = require('@osmonauts/telescope').default;
+
+const protoDirs = [join(__dirname, '/../proto')];
+
+telescope({
+  protoDirs,
+  outPath: join(__dirname, '../codegen'),
+  options: {
+    tsDisable: {
+      files: [
+        'ibc/core/types/v1/genesis.ts',
+        'google/protobuf/descriptor.ts',
+        'google/protobuf/struct.ts'
+      ]
+    },
+    prototypes: {
+      allowUndefinedTypes: true,
+      fieldDefaultIsOptional: true,
+      includePackageVar: false,
+      typingsFormat: {
+        useDeepPartial: false,
+        useExact: false,
+        num64: "bigint",
+        timestamp: 'date',
+        duration: 'duration'
+      },
+    },
+    aminoEncoding: {
+      enabled: true
+    },
+    lcdClients: {
+      enabled: true
+    },
+    bundle: {
+        enabled: true
+    },
+    stargateClients: {
+        enabled: true,
+        includeCosmosDefaultTypes: true
+    },
+    rpcClients: {
+      type: 'grpc-gateway',
+      enabled: true,
+      camelCase: true
+    },
+    // need new query extension from telescope for this to work, don't enable it for now
+    reactQuery: {
+      enabled: false
+    },
+    mobx: {
+      enabled: false
+    }
+  }
+}).then(() => {
+  console.log('✨ all done!');
+}).catch(e=>{
+  console.error(e);
+});
