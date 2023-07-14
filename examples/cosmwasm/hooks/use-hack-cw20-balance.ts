768c465d9f91fecd08917a3a79e74a610320e376
Dan Lynch <pyramation@gmail.com>
2023-07-14 16:34:23 +0200

    cosmwasm example

--- examples/cosmwasm/hooks/use-hack-cw20-balance.ts
+++ examples/cosmwasm/hooks/use-hack-cw20-balance.ts
@@ -1,36 +1,31 @@
 import { useState, useEffect } from 'react';
 import { useChain } from '@cosmos-kit/react';
-
-// import cosmwasm client generated with cosmwasm-ts-codegen
-import { HackCw20QueryClient } from '../codegen/HackCw20.client';
 import { chainName } from '../config';
+
+import { useContracts } from '../codegen/contracts-context';
 
 export function useHackCw20Balance(contractAddress: string): {
   balance: string | undefined;
 } {
-  const { getSigningCosmWasmClient, address, status } = useChain(chainName);
+  const {
+    address,
+    status
+  } = useChain(chainName);
 
-  const [cw20Client, setCw20Client] = useState<HackCw20QueryClient | null>(
-    null
-  );
+  const { hackCw20 } = useContracts()
+
   useEffect(() => {
     if (status === 'Connected') {
-      getSigningCosmWasmClient().then((cosmwasmClient) => {
-        if (!cosmwasmClient || !address) {
-          console.error('cosmwasmClient undefined or address undefined.');
-          return;
-        }
 
-        setCw20Client(new HackCw20QueryClient(cosmwasmClient, contractAddress));
-      });
     }
   }, [address, contractAddress, status]);
   const [cw20Bal, setCw20Bal] = useState<string | null>(null);
   useEffect(() => {
-    if (cw20Client && address) {
-      cw20Client.balance({ address }).then((b) => setCw20Bal(b.balance));
+    if (hackCw20 && address) {
+      // hackCw20.balance({ address }).then((b) => setCw20Bal(b.balance));
+      hackCw20.getQueryClient(contractAddress)
     }
-  }, [cw20Client, address]);
+  }, [hackCw20, address]);
 
   return { balance: cw20Bal ?? undefined };
 }
