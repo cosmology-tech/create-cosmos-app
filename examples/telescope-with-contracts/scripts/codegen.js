457f0e59dbb72e95aeb40e27b21a564c25b9f729
Zetazzz <zetazz@163.com>
2023-07-14 12:08:24 +0800

    upgrade telescope and re-gen code

--- examples/telescope-with-contracts/scripts/codegen.js
+++ examples/telescope-with-contracts/scripts/codegen.js
@@ -79,6 +79,9 @@ telescope({
         messageComposer: {
           enabled: true,
         },
+        useContractsHooks: {
+          enabled: true
+        }
       },
     },
     aminoEncoding: {
