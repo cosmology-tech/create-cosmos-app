69bb35acb09873fdf344f41b9f55da2eb11db599
Tom Wang <tom.wanghaiyi@gmail.com>
2023-10-27 21:15:42 +0800

    Refactor utils

--- examples/swap-tokens-new/utils/utils.ts
+++ examples/swap-tokens-new/utils/utils.ts
@@ -1,16 +1,16 @@
 import BigNumber from 'bignumber.js';
 import { chains } from 'chain-registry';
-import { Asset } from '@chain-registry/types';
+import { Asset, Chain } from '@chain-registry/types';
 
 export function integer(number: number | string) {
   return new BigNumber(number).decimalPlaces(0, BigNumber.ROUND_DOWN).toString();
 }
 
-export function getAssetLogo(asset: Asset) {
-  return asset.logo_URIs?.svg || asset.logo_URIs?.png || asset.logo_URIs?.jpeg;
+export function getLogo(from: Asset | Chain) {
+  return from.logo_URIs?.svg || from.logo_URIs?.png || from.logo_URIs?.jpeg;
 }
 
 export function getChainLogo(name: string) {
   const chain = chains.find(chain => chain.chain_name === name)
-  return chain ? chain.logo_URIs?.svg || chain.logo_URIs?.png || chain.logo_URIs?.jpeg : ''
+  return chain ? getLogo(chain) : null;
 }
