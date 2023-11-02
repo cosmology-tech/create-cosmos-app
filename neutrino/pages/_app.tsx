3a73a71f183aff6e2aaa3cd669fe75cad49acc05
mconcat <monoidconcat@gmail.com>
2023-11-02 12:08:39 +0900

    feat: add neutrino

---
+++ neutrino/pages/_app.tsx
@@ -0,0 +1,47 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChainProvider } from '@cosmos-kit/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { wallets as keplrWallets } from '@cosmos-kit/keplr';
+import { wallets as cosmostationWallets } from '@cosmos-kit/cosmostation';
+import { wallets as leapWallets } from '@cosmos-kit/leap';
+
+import { SignerOptions } from '@cosmos-kit/core';
+import { chains, assets } from 'chain-registry';
+import { defaultTheme } from '../config';
+import '@interchain-ui/react/styles';
+
+function CreateCosmosApp({ Component, pageProps }: AppProps) {
+  const signerOptions: SignerOptions = {
+    // signingStargate: () => {
+    //   return getSigningCosmosClientOptions();
+    // }
+  };
+
+  return (
+    <ChakraProvider theme={defaultTheme}>
+      <ChainProvider
+        chains={chains}
+        assetLists={assets}
+        wallets={[...keplrWallets, ...cosmostationWallets, ...leapWallets]}
+        walletConnectOptions={{
+          signClient: {
+            projectId: 'a8510432ebb71e6948cfd6cde54b70f7',
+            relayUrl: 'wss://relay.walletconnect.org',
+            metadata: {
+              name: 'CosmosKit Template',
+              description: 'CosmosKit dapp template',
+              url: 'https://docs.cosmoskit.com/',
+              icons: [],
+            },
+          },
+        }}
+        signerOptions={signerOptions}
+      >
+        <Component {...pageProps} />
+      </ChainProvider>
+    </ChakraProvider>
+  );
+}
+
+export default CreateCosmosApp;
