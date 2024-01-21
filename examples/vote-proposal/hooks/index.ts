d4d68aa908e171ddaa584546e7c00ef67c77cf28
Tom Wang <tom.wanghaiyi@gmail.com>
2024-01-21 12:54:37 +0800

    Vote proposal example with interchain ui

---
+++ examples/vote-proposal/hooks/index.ts
@@ -0,0 +1,6 @@
+export * from './useTx';
+export * from './useModal';
+export * from './useVoting';
+export * from './useVotingData';
+export * from './useQueryHooks';
+export * from './useRpcQueryClient';
