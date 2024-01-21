382e22e678627e7793ea588a3ea2c1d446c14958
Tom Wang <tom.wanghaiyi@gmail.com>
2024-01-21 11:59:20 +0800

    Fix proposal title

--- examples/vote-proposal-chakra/components/voting/ProposalCard.tsx
+++ examples/vote-proposal-chakra/components/voting/ProposalCard.tsx
@@ -1,7 +1,7 @@
-import React, { useMemo } from 'react';
-import { Proposal } from 'interchain-query/cosmos/gov/v1beta1/gov';
-import { cosmos } from 'interchain-query';
-import dayjs from 'dayjs';
+import React, { useMemo } from "react";
+import { Proposal } from "interchain-query/cosmos/gov/v1beta1/gov";
+import { cosmos } from "interchain-query";
+import dayjs from "dayjs";
 import {
   Box,
   Center,
@@ -12,19 +12,19 @@ import {
   Text,
   useColorMode,
   useColorModeValue,
-} from '@chakra-ui/react';
+} from "@chakra-ui/react";
 
-import { Votes } from '@/hooks';
-import { decodeUint8Arr, getPercentage, getTitleFromDecoded } from '@/utils';
-import { StatusBadge, VotedBadge, VoteOption } from './common';
+import { Votes } from "@/hooks";
+import { decodeUint8Arr, getPercentage, getTitleFromDecoded } from "@/utils";
+import { StatusBadge, VotedBadge, VoteOption } from "./common";
 
 const ProposalStatus = cosmos.gov.v1beta1.ProposalStatus;
 
 export const VoteColor: { [key in VoteOption]: string } = {
-  [VoteOption.YES]: '#17a572',
-  [VoteOption.NO]: '#ce4256',
-  [VoteOption.NWV]: '#ff5b6d',
-  [VoteOption.ABSTAIN]: '#546198',
+  [VoteOption.YES]: "#17a572",
+  [VoteOption.NO]: "#ce4256",
+  [VoteOption.NWV]: "#ff5b6d",
+  [VoteOption.ABSTAIN]: "#546198",
 };
 
 export const ProposalCard = ({
@@ -42,7 +42,7 @@ export const ProposalCard = ({
     if (!proposal.finalTallyResult) return 0;
     const total = Object.values(proposal.finalTallyResult).reduce(
       (prev, cur) => prev + Number(cur),
-      0
+      0,
     );
     return total ? total : 0;
   }, [proposal]);
@@ -54,88 +54,94 @@ export const ProposalCard = ({
       h="120px"
       py={4}
       templateColumns="repeat(13, 1fr)"
-      bgColor={useColorModeValue('#fbfcfe', 'whiteAlpha.200')}
+      bgColor={useColorModeValue("#fbfcfe", "whiteAlpha.200")}
       borderColor="gray.400"
       borderRadius={10}
       transition="all 0.2s linear"
       _hover={{
-        backgroundColor: useColorModeValue('gray.100', 'gray.700'),
-        cursor: 'pointer',
+        backgroundColor: useColorModeValue("gray.100", "gray.700"),
+        cursor: "pointer",
       }}
       onClick={handleClick}
     >
       <GridItem colSpan={2}>
         <Center w="100%" h="100%">
-          # {proposal.proposalId.toString().padStart(6, '0')}
+          # {proposal.proposalId.toString().padStart(6, "0")}
         </Center>
       </GridItem>
       <GridItem colSpan={9} py={2}>
         <Flex flexDirection="column" h="100%">
           <Flex gap={2} alignItems="center">
             <Text fontSize="lg">
-              {getTitleFromDecoded(decodeUint8Arr(proposal.content?.value))}
+              {proposal.content?.title ||
+                getTitleFromDecoded(decodeUint8Arr(proposal.content?.value))}
             </Text>
             {isVoted && <VotedBadge />}
           </Flex>
           <Spacer />
           <Flex flexDirection="column" h="44%">
             <Flex alignItems="center" fontSize="sm">
-              <Text color={useColorModeValue('gray.600', 'gray.400')}>
+              <Text color={useColorModeValue("gray.600", "gray.400")}>
                 {proposal.status ===
-                ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD
-                  ? 'Deposit'
-                  : 'Voting'}
+                    ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD
+                  ? "Deposit"
+                  : "Voting"}
                 &nbsp;end time: &nbsp;
               </Text>
               <Text
-                color={useColorModeValue('gray.600', 'gray.400')}
+                color={useColorModeValue("gray.600", "gray.400")}
                 fontWeight="semibold"
               >
                 {dayjs(
                   proposal.status ===
-                    ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD
+                      ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD
                     ? proposal.depositEndTime
-                    : proposal.votingEndTime
-                ).format('YYYY-MM-DD hh:mm')}
+                    : proposal.votingEndTime,
+                ).format("YYYY-MM-DD hh:mm")}
               </Text>
             </Flex>
             <Spacer />
-            {totalVotes ? (
-              <Flex gap="1px">
+            {totalVotes
+              ? (
+                <Flex gap="1px">
+                  <Box
+                    w={getPercentage(
+                      proposal.finalTallyResult?.yes,
+                      totalVotes,
+                    )}
+                    h="3px"
+                    bgColor={VoteColor.YES}
+                  />
+                  <Box
+                    w={getPercentage(proposal.finalTallyResult?.no, totalVotes)}
+                    h="3px"
+                    bgColor={VoteColor.NO}
+                  />
+                  <Box
+                    w={getPercentage(
+                      proposal.finalTallyResult?.noWithVeto,
+                      totalVotes,
+                    )}
+                    h="3px"
+                    bgColor={VoteColor.NWV}
+                  />
+                  <Box
+                    w={getPercentage(
+                      proposal.finalTallyResult?.abstain,
+                      totalVotes,
+                    )}
+                    h="3px"
+                    bgColor={VoteColor.ABSTAIN}
+                  />
+                </Flex>
+              )
+              : (
                 <Box
-                  w={getPercentage(proposal.finalTallyResult?.yes, totalVotes)}
+                  w="100%"
                   h="3px"
-                  bgColor={VoteColor.YES}
+                  bgColor={colorMode === "light" ? "gray.200" : "gray.600"}
                 />
-                <Box
-                  w={getPercentage(proposal.finalTallyResult?.no, totalVotes)}
-                  h="3px"
-                  bgColor={VoteColor.NO}
-                />
-                <Box
-                  w={getPercentage(
-                    proposal.finalTallyResult?.noWithVeto,
-                    totalVotes
-                  )}
-                  h="3px"
-                  bgColor={VoteColor.NWV}
-                />
-                <Box
-                  w={getPercentage(
-                    proposal.finalTallyResult?.abstain,
-                    totalVotes
-                  )}
-                  h="3px"
-                  bgColor={VoteColor.ABSTAIN}
-                />
-              </Flex>
-            ) : (
-              <Box
-                w="100%"
-                h="3px"
-                bgColor={colorMode === 'light' ? 'gray.200' : 'gray.600'}
-              />
-            )}
+              )}
           </Flex>
         </Flex>
       </GridItem>
