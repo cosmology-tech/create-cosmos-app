1f9ff4cbf8aba5deec9a7ed5697d504b84e00a2d
Tom Wang <tom.wanghaiyi@gmail.com>
2024-01-22 13:09:39 +0800

    Use latest ChangeChainCombobox

--- examples/vote-proposal/components/wallet/Chain.tsx
+++ examples/vote-proposal/components/wallet/Chain.tsx
@@ -1,26 +1,32 @@
-import { Chain } from '@chain-registry/types';
-import { ChangeChainCombobox } from '@interchain-ui/react';
-import { getChainLogo } from '@/utils';
+import { Chain } from "@chain-registry/types";
+import { ChangeChainCombobox } from "@interchain-ui/react";
+import { getChainLogo } from "@/utils";
 
 export type ChainProps = {
   name: string;
-  chains: Chain[]
+  chains: Chain[];
   onChainChange?: (chainName?: string) => void;
-}
+};
 
-export function Chain({ name, chains = [], onChainChange = () => {} }: ChainProps) {
-  const options = chains.map(chain => ({
+export function Chain(
+  { name, chains = [], onChainChange = () => {} }: ChainProps,
+) {
+  const options = chains.map((chain) => ({
+    label: chain.pretty_name,
+    value: chain.chain_name,
     iconUrl: getChainLogo(chain),
-    chainName: chain.chain_name,
   }));
 
-  const value = options.find(chain => chain.chainName === name)!;
-  
-  return <ChangeChainCombobox
-    size="md"
-    maxHeight={360}
-    options={options}
-    valueItem={value}
-    onItemSelected={(option) => onChainChange(option?.chainName)}
-  />
+  const value = options.find((chain) => chain.value === name)!;
+
+  return (
+    <ChangeChainCombobox
+      size="md"
+      maxHeight={360}
+      options={options}
+      valueItem={value}
+      appearance="bold"
+      onItemSelected={(option) => onChainChange(option?.value)}
+    />
+  );
 }
