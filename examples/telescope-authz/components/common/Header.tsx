4a34d7010b3b8b7f05d157a9d30e0bb8cc1b5ed0
luca <uniqueye608@gmail.com>
2024-01-24 22:06:19 +0800

    feat: create authz example layout

---
+++ examples/telescope-authz/components/common/Header.tsx
@@ -0,0 +1,63 @@
+import {
+  Box,
+  Button,
+  Icon,
+  Text,
+  useTheme,
+  useColorModeValue,
+} from '@interchain-ui/react';
+
+const stacks = ['CosmosKit', 'Next.js'];
+
+export function Header() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleColorMode = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <>
+      <Box display="flex" justifyContent="end" mb="$8">
+        <Button
+          intent="secondary"
+          size="sm"
+          attributes={{
+            paddingX: 0,
+          }}
+          onClick={toggleColorMode}
+        >
+          <Icon name={useColorModeValue('moonLine', 'sunLine')} />
+        </Button>
+      </Box>
+
+      <Box textAlign="center">
+        <Text
+          as="h1"
+          fontWeight="$extrabold"
+          fontSize={{ mobile: '$6xl', tablet: '$10xl' }}
+          attributes={{
+            marginBottom: '$2',
+          }}
+        >
+          Create Cosmos App
+        </Text>
+        <Text as="h2" fontWeight="$bold">
+          <Text
+            as="span"
+            fontSize={{ mobile: '$3xl', tablet: '$8xl', desktop: '$8xl' }}
+          >
+            Welcome to&nbsp;
+          </Text>
+          <Text
+            as="span"
+            fontSize={{ mobile: '$3xl', tablet: '$8xl', desktop: '$8xl' }}
+            color={useColorModeValue('$primary500', '$primary200')}
+          >
+            {stacks.join(' + ')}
+          </Text>
+        </Text>
+      </Box>
+    </>
+  );
+}
---
+++ examples/telescope-authz/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@interchain-ui/react';
+import { Layout } from '@/components';
+
+export default function Home() {
+  return (
+    <Layout>
+      <Box maxWidth="$containerMd" mb="$30"></Box>
+    </Layout>
+  );
+}
