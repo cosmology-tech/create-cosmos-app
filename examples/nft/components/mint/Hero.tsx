a769b859271da355dd49d33a2b7a2e0e348dd9f8
Tom Wang <tom.wanghaiyi@gmail.com>
2024-01-25 15:48:31 +0800

    Use skeleton for loading

--- examples/nft/components/mint/Hero.tsx
+++ examples/nft/components/mint/Hero.tsx
@@ -1,13 +1,7 @@
 import Image from "next/image";
 import { ellipsis, ipfs } from "@/utils";
 import { Collection, useTokens } from "@/hooks/mint";
-import {
-  Box,
-  Button,
-  Spinner,
-  Text,
-  useColorModeValue,
-} from "@interchain-ui/react";
+import { Box, Button, Skeleton, Text } from "@interchain-ui/react";
 
 export type HeroProps = {
   collection: Partial<Collection> | null;
@@ -16,49 +10,48 @@ export type HeroProps = {
 
 export function Hero({ collection, onMint = () => {} }: HeroProps) {
   const { tokens } = useTokens();
+  const positions = [
+    { top: "40px" },
+    { left: "160px", top: "-55px", zIndex: "0.4" },
+    { right: "0", top: "80px", zIndex: "0.3" },
+    { bottom: "-55px", left: "130px", zIndex: "0.2" },
+  ];
   const images = tokens.length > 0
-    ? [
-      {
-        url: tokens[0].imageUrl!,
-        css: { top: "40px" },
-      },
-      {
-        url: tokens[1].imageUrl!,
-        css: { left: "160px", top: "-55px", zIndex: "0.4" },
-      },
-      {
-        url: tokens[2].imageUrl!,
-        css: { right: "0", top: "80px", zIndex: "0.3" },
-      },
-      {
-        url: tokens[3].imageUrl!,
-        css: { bottom: "-55px", left: "130px", zIndex: "0.2" },
-      },
-    ]
+    ? tokens.map((token, i) => ({
+      url: token.imageUrl!,
+      css: positions[i],
+    }))
     : [];
 
-  const Loading = (
+  const TitleSkeleton = (
     <Box
-      mb="$16"
-      boxShadow="$sm"
-      minHeight="20rem"
+      mb="$12"
       display="flex"
-      alignItems="center"
-      justifyContent="center"
-      borderWidth="1px"
-      borderRadius="$lg"
-      borderStyle="$solid"
-      borderColor="$divider"
+      flexDirection="column"
+      gap="$6"
     >
-      <Spinner
-        size="$5xl"
-        color={useColorModeValue("$blackAlpha800", "$whiteAlpha900")}
+      <Skeleton
+        mb="$6"
+        borderRadius="$sm"
+        height="$14"
+        width="$28"
+      />
+      <Skeleton
+        mb="$2"
+        borderRadius="$sm"
+        height="$12"
+        width="$32"
+      />
+      <Skeleton
+        borderRadius="$sm"
+        height="$12"
+        width="$30"
       />
     </Box>
   );
 
   return (
-    collection?.isLoading ? Loading : collection?.address
+    collection?.address
       ? (
         <Box
           width="$full"
@@ -70,23 +63,27 @@ export function Hero({ collection, onMint = () => {} }: HeroProps) {
           display="flex"
         >
           <Box flex="1">
-            <Text
-              fontSize="3rem"
-              fontWeight="600"
-              lineHeight="shorter"
-              attributes={{ display: "block" }}
-            >
-              {collection.contract?.name}
-            </Text>
-            <Text
-              fontSize="1rem"
-              fontWeight="400"
-              lineHeight="shorter"
-              color="$textSecondary"
-              attributes={{ mt: "$10", mb: "$14" }}
-            >
-              {ellipsis(collection.info?.description)}
-            </Text>
+            {collection.isLoading ? TitleSkeleton : (
+              <Box>
+                <Text
+                  fontSize="3rem"
+                  fontWeight="600"
+                  lineHeight="shorter"
+                  attributes={{ display: "block" }}
+                >
+                  {collection.contract?.name}
+                </Text>
+                <Text
+                  fontSize="1rem"
+                  fontWeight="400"
+                  lineHeight="shorter"
+                  color="$textSecondary"
+                  attributes={{ mt: "$10", mb: "$14" }}
+                >
+                  {ellipsis(collection.info?.description)}
+                </Text>
+              </Box>
+            )}
             <Button
               size="lg"
               intent="tertiary"
@@ -96,22 +93,26 @@ export function Hero({ collection, onMint = () => {} }: HeroProps) {
               Mint Now
             </Button>
           </Box>
-          <Box position="relative" flex="1">
-            {images.map(({ url, css }) => (
-              <Image
-                alt={""}
-                key={url}
-                src={ipfs(url)}
-                width="180"
-                height="180"
-                style={{
-                  ...css,
-                  borderRadius: "6px",
-                  position: "absolute",
-                }}
-              />
-            ))}
-          </Box>
+          {collection.isLoading
+            ? <Skeleton width="23rem" height="23rem" borderRadius="$md" />
+            : (
+              <Box position="relative" flex="1">
+                {images.map(({ url, css }) => (
+                  <Image
+                    alt={""}
+                    key={url}
+                    src={ipfs(url)}
+                    width="180"
+                    height="180"
+                    style={{
+                      ...css,
+                      borderRadius: "6px",
+                      position: "absolute",
+                    }}
+                  />
+                ))}
+              </Box>
+            )}
         </Box>
       )
       : null
