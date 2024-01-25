15533fec8cb0f9cee1c20acdfdee1730ab2a6329
luca <uniqueye608@gmail.com>
2024-01-25 21:24:32 +0800

    feat: add wallet section

--- examples/telescope-authz/pages/index.tsx
+++ examples/telescope-authz/pages/index.tsx
@@ -1,10 +1,22 @@
-import { Box } from '@interchain-ui/react';
-import { Layout } from '@/components';
+import { useState } from 'react';
+import { Divider } from '@interchain-ui/react';
+import { ChainName } from 'cosmos-kit';
+import NoSSR from 'react-no-ssr';
+
+import { Layout, WalletSection } from '@/components';
 
 export default function Home() {
+  const [selectedChain, setSelectedChain] = useState<ChainName>();
+
   return (
     <Layout>
-      <Box maxWidth="$containerMd" mb="$30"></Box>
+      <WalletSection
+        isMultiChain={true}
+        providedChainName={selectedChain}
+        setChainName={setSelectedChain}
+      />
+      <Divider height="0.1px" mt="$12" mb="$17" />
+      <NoSSR></NoSSR>
     </Layout>
   );
 }
