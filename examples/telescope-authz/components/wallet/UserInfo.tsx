62a47648ebf0421db397b0ca67082280b90ec5b2
luca <uniqueye608@gmail.com>
2024-01-25 21:24:32 +0800

    feat: add wallet section

---
+++ examples/telescope-authz/components/wallet/UserInfo.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { Text, Stack, Box } from '@interchain-ui/react';
+
+interface IUserInfo {
+  walletIcon?: string;
+  username?: string;
+  icon?: React.ReactNode;
+}
+
+export const UserInfo = ({ username, icon }: IUserInfo) => {
+  return (
+    <Stack
+      direction="vertical"
+      space={1}
+      attributes={{
+        alignItems: 'center',
+      }}
+    >
+      {username && (
+        <>
+          <Box
+            display={icon ? 'block' : 'none'}
+            width={{
+              mobile: '$16',
+              tablet: '$19',
+            }}
+            height={{
+              mobile: '$16',
+              tablet: '$19',
+            }}
+            borderRadius="$full"
+            overflow="hidden"
+          >
+            {icon}
+          </Box>
+
+          <Text
+            fontSize={{ mobile: '$sm', tablet: '$xl' }}
+            fontWeight="$semibold"
+          >
+            {username}
+          </Text>
+        </>
+      )}
+    </Stack>
+  );
+};
