d07f863e567f00baa3ce8dd40251668704f55496
luca <uniqueye608@gmail.com>
2024-01-29 14:48:32 +0800

    fix: chain dropdown doesn't show chain names

--- examples/telescope-authz/pages/index.tsx
+++ examples/telescope-authz/pages/index.tsx
@@ -2,18 +2,14 @@ import { useState } from 'react';
 import { Divider } from '@interchain-ui/react';
 import { ChainName } from 'cosmos-kit';
 
-import { Layout, WalletSection, AuthzSection } from '@/components';
+import { Layout, Wallet, AuthzSection } from '@/components';
 
 export default function Home() {
   const [selectedChain, setSelectedChain] = useState<ChainName>();
 
   return (
     <Layout>
-      <WalletSection
-        isMultiChain={true}
-        providedChainName={selectedChain}
-        setChainName={setSelectedChain}
-      />
+      <Wallet chainName={selectedChain} onChainChange={setSelectedChain} />
       <Divider height="0.1px" mt="$12" mb="$17" />
       {selectedChain && <AuthzSection chainName={selectedChain} />}
     </Layout>
--- examples/telescope-authz/utils/index.ts
+++ examples/telescope-authz/utils/index.ts
@@ -1,7 +1,12 @@
+import { Chain } from '@chain-registry/types';
 import { assets } from 'chain-registry';
 
-export const getChainLogoUrl = (chainName: string): string => {
+export const getChainLogoByChainName = (chainName: string): string => {
   const asset = assets.find(({ chain_name }) => chain_name === chainName)
     ?.assets?.[0];
   return Object.values(asset?.logo_URIs || {})?.[0] || '';
+};
+
+export const getChainLogoFromChain = (chain: Chain) => {
+  return Object.values(chain?.logo_URIs || {})?.[0] || '';
 };
