b505cb7761e9f1964bd2eed7e2c323259b984a34
luca <uniqueye608@gmail.com>
2024-02-01 23:54:07 +0800

    fix: use cosmos-kit in vote-proposal

--- examples/vote-proposal/pages/_app.tsx
+++ examples/vote-proposal/pages/_app.tsx
@@ -1,16 +1,19 @@
 import type { AppProps } from 'next/app';
+import { SignerOptions, wallets } from 'cosmos-kit';
 import { ChainProvider } from '@cosmos-kit/react';
-import { wallets as keplr } from '@cosmos-kit/keplr';
-import { wallets as cosmostation } from '@cosmos-kit/cosmostation';
-import { wallets as leap } from '@cosmos-kit/leap';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { Box, ThemeProvider, Toaster, useTheme, useColorModeValue } from '@interchain-ui/react';
-import '@interchain-ui/react/styles';
+import {
+  Box,
+  ThemeProvider,
+  Toaster,
+  useTheme,
+  useColorModeValue,
+} from '@interchain-ui/react';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-import { SignerOptions } from '@cosmos-kit/core';
 import { chains, assets } from 'chain-registry';
-import '../styles/globals.css';
+
+import '@interchain-ui/react/styles';
+import '@interchain-ui/react/globalStyles';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,7 +38,7 @@ function CreateCosmosApp({ Component, pageProps }: AppProps) {
       <ChainProvider
         chains={chains}
         assetLists={assets}
-        wallets={[...keplr, ...leap]}
+        wallets={wallets}
         walletConnectOptions={{
           signClient: {
             projectId: 'a8510432ebb71e6948cfd6cde54b70f7',
@@ -51,7 +54,11 @@ function CreateCosmosApp({ Component, pageProps }: AppProps) {
         signerOptions={signerOptions}
       >
         <QueryClientProvider client={queryClient}>
-          <Box className={themeClass} minHeight="100dvh" backgroundColor={useColorModeValue('$white', '$background')}>
+          <Box
+            className={themeClass}
+            minHeight="100dvh"
+            backgroundColor={useColorModeValue('$white', '$background')}
+          >
             <Component {...pageProps} />
           </Box>
         </QueryClientProvider>
