9cc3d24055cc54358af9dc7d8a56856bd2ef0787
yyyyaaa <phatghaa@gmail.com>
2024-02-05 09:27:41 +0700

    fix: custom filtering stake-tokens

--- examples/asset-list/next.config.js
+++ examples/asset-list/next.config.js
@@ -1,7 +1,19 @@
+// const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // Workaround for locally linked packages with yarn link/npm link
+  // webpack: (config, _context) => {
+  //   const overridePath = path.resolve(
+  //     __dirname,
+  //     "../..",
+  //     "node_modules/react"
+  //   );
+  //   config.resolve.alias["react"] = overridePath;
+  //   return config;
+  // },
 }
 
 module.exports = nextConfig
