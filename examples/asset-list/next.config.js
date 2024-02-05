19d8a7c1bb7d7b3799b8505d49026e60623a36df
yyyyaaa <phatghaa@gmail.com>
2024-02-05 09:39:05 +0700

    chore: remove unused config

--- examples/asset-list/next.config.js
+++ examples/asset-list/next.config.js
@@ -1,19 +1,7 @@
-// const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Workaround for locally linked packages with yarn link/npm link
-  // webpack: (config, _context) => {
-  //   const overridePath = path.resolve(
-  //     __dirname,
-  //     "../..",
-  //     "node_modules/react"
-  //   );
-  //   config.resolve.alias["react"] = overridePath;
-  //   return config;
-  // },
 }
 
 module.exports = nextConfig
