11d9b6282c796ca6931fd61f9757577278a7a8d0
yyyyaaa <phatghaa@gmail.com>
2024-06-21 11:01:00 +0700

    fix: dependencies

--- templates/connect-multi-chain/next.config.js
+++ templates/connect-multi-chain/next.config.js
@@ -1,6 +1,39 @@
-/** @type {import('next').NextConfig} */
+// const path = require('path');
 
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  // webpack: (config, options) => {
+  //   if (options.isServer) {
+  //     config.externals = ['react', ...config.externals];
+  //   }
+
+  //   config.resolve.alias['react'] = path.resolve(
+  //     __dirname,
+  //     '.',
+  //     'node_modules',
+  //     'react'
+  //   );
+  //   config.resolve.alias['react-dom'] = path.resolve(
+  //     __dirname,
+  //     '.',
+  //     'node_modules',
+  //     'react-dom'
+  //   );
+  //   config.resolve.alias['@interchain-ui/react'] = path.resolve(
+  //     __dirname,
+  //     '.',
+  //     'node_modules',
+  //     '@interchain-ui/react/dist/interchain-ui-kit-react.cjs.js'
+  //   );
+
+  //   config.resolve.alias['@interchain-ui/react/styles'] = path.resolve(
+  //     __dirname,
+  //     '.',
+  //     'node_modules',
+  //     '@interchain-ui/react/dist/interchain-ui-kit-react.cjs.css'
+  //   );
+
+  //   return config;
+  // },
 };
