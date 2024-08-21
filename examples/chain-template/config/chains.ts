9a04772bc87d2b09bb19ce3e57b51d5f525e9488
Eason Smith <eason@smi.th>
2024-08-21 10:56:57 +0800

    added cosmoshub chain

--- examples/chain-template/config/chains.ts
+++ examples/chain-template/config/chains.ts
@@ -1,7 +1,7 @@
 import { chains } from 'chain-registry';
 import osmosis from 'chain-registry/mainnet/osmosis/chain';
 
-const chainNames = ['osmosistestnet', 'juno', 'stargaze', 'osmosis'];
+const chainNames = ['osmosistestnet', 'juno', 'stargaze', 'osmosis', 'cosmoshub'];
 
 export const chainOptions = chainNames.map(
   (chainName) => chains.find((chain) => chain.chain_name === chainName)!
