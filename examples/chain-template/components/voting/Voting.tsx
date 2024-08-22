8476be5838a9d53e0452915c88aecd73978ce585
luca <uniqueye608@gmail.com>
2024-08-22 13:06:44 +0800

    fix(chain-template): change text and styles

--- examples/chain-template/components/voting/Voting.tsx
+++ examples/chain-template/components/voting/Voting.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import { useChain } from "@cosmos-kit/react";
+import { useState } from 'react';
+import { useChain } from '@cosmos-kit/react';
 import {
   Proposal as IProposal,
   ProposalStatus,
   TallyResult,
-} from "interchain-query/cosmos/gov/v1/gov";
+} from 'interchain-query/cosmos/gov/v1/gov';
 import {
   BasicModal,
   Box,
@@ -12,27 +12,27 @@ import {
   Spinner,
   Text,
   useColorModeValue,
-} from "@interchain-ui/react";
-import { useModal, useVotingData } from "@/hooks";
-import { Proposal } from "@/components";
-import { formatDate } from "@/utils";
+} from '@interchain-ui/react';
+import { useModal, useVotingData } from '@/hooks';
+import { Proposal } from '@/components';
+import { formatDate } from '@/utils';
 
 function status(s: ProposalStatus) {
   switch (s) {
     case ProposalStatus.PROPOSAL_STATUS_UNSPECIFIED:
-      return "pending";
+      return 'pending';
     case ProposalStatus.PROPOSAL_STATUS_DEPOSIT_PERIOD:
-      return "pending";
+      return 'pending';
     case ProposalStatus.PROPOSAL_STATUS_VOTING_PERIOD:
-      return "pending";
+      return 'pending';
     case ProposalStatus.PROPOSAL_STATUS_PASSED:
-      return "passed";
+      return 'passed';
     case ProposalStatus.PROPOSAL_STATUS_REJECTED:
-      return "rejected";
+      return 'rejected';
     case ProposalStatus.PROPOSAL_STATUS_FAILED:
-      return "rejected";
+      return 'rejected';
     default:
-      return "pending";
+      return 'pending';
   }
 }
 
@@ -53,7 +53,7 @@ export function Voting({ chainName }: VotingProps) {
   const { address } = useChain(chainName);
   const [proposal, setProposal] = useState<IProposal>();
   const { data, isLoading, refetch } = useVotingData(chainName);
-  const { modal, open: openModal, close: closeModal, setTitle } = useModal("");
+  const { modal, open: openModal, close: closeModal, setTitle } = useModal('');
 
   function onClickProposal(index: number) {
     const proposal = data.proposals![index];
@@ -72,23 +72,21 @@ export function Voting({ chainName }: VotingProps) {
           position="relative"
           attributes={{ onClick: () => onClickProposal(index) }}
         >
-          {data.votes[proposal.id.toString()]
-            ? (
-              <Box
-                position="absolute"
-                px="$4"
-                py="$2"
-                top="$4"
-                right="$6"
-                borderRadius="$md"
-                backgroundColor="$green400"
-              >
-                <Text color="$white" fontSize="$xs" fontWeight="$bold">
-                  Voted
-                </Text>
-              </Box>
-            )
-            : null}
+          {data.votes[proposal.id.toString()] ? (
+            <Box
+              position="absolute"
+              px="$4"
+              py="$2"
+              top="$4"
+              right="$6"
+              borderRadius="$md"
+              backgroundColor="$green400"
+            >
+              <Text color="$white" fontSize="$xs" fontWeight="$bold">
+                Voted
+              </Text>
+            </Box>
+          ) : null}
           <GovernanceProposalItem
             id={`# ${proposal.id?.toString()}`}
             key={proposal.submitTime?.getTime()}
@@ -116,18 +114,20 @@ export function Voting({ chainName }: VotingProps) {
       p="$8"
       borderRadius="$md"
       justifyContent="center"
-      display={isLoading ? "flex" : "none"}
+      display={isLoading ? 'flex' : 'none'}
     >
       <Spinner
         size="$5xl"
-        color={useColorModeValue("$blackAlpha800", "$whiteAlpha900")}
+        color={useColorModeValue('$blackAlpha800', '$whiteAlpha900')}
       />
     </Box>
   );
 
   return (
-    <Box mb="$20" position="relative">
-      <Text fontWeight="600" fontSize="$2xl">Proposals</Text>
+    <Box mb="$20" position="relative" maxWidth="$containerLg" mx="auto">
+      <Text fontWeight="600" fontSize="$2xl">
+        Proposals
+      </Text>
 
       {address ? Loading : null}
 
@@ -136,7 +136,9 @@ export function Voting({ chainName }: VotingProps) {
       <BasicModal
         title={
           <Box maxWidth="40rem">
-            <Text fontSize="$xl" fontWeight="$bold">{modal.title}</Text>
+            <Text fontSize="$xl" fontWeight="$bold">
+              {modal.title}
+            </Text>
           </Box>
         }
         isOpen={modal.open}
