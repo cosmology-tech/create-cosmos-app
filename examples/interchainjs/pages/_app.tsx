06331b75411ce31c86be4915d996915f465ebbcf
Eason Smith <eason@smi.th>
2024-09-09 09:19:42 +0800

    remove unused code

--- examples/interchainjs/pages/_app.tsx
+++ examples/interchainjs/pages/_app.tsx
@@ -5,14 +5,7 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-import { SignerOptions, wallets } from 'cosmos-kit';
-// import { ChainProvider } from '@cosmos-kit/react';
-// import { chains, assets } from 'chain-registry';
 import { assetLists, chains } from '@chain-registry/v2';
-import { getSigningCosmosClientOptions } from '../src/codegen';
-import { GasPrice } from '@cosmjs/stargate';
 import { ChainProvider } from '@interchain-kit/react'
 import { keplrWallet } from '@interchain-kit/keplr-extension'
 
@@ -20,61 +13,32 @@ import { defaultTheme } from '../config';
 
 const queryClient = new QueryClient();
 
+const chainNames = ['cosmoshub']
+const _chains = chains.filter(c => chainNames.includes(c.chainName))
+const _assetLists = assetLists.filter(a => chainNames.includes(a.chainName))
+
 function CreateCosmosApp({ Component, pageProps }: AppProps) {
-  // const signerOptions: SignerOptions = {
-  //   signingStargate: () => {
-  //     return getSigningCosmosClientOptions();
-  //   },
-  //   signingCosmwasm: (chain) => {
-  //     switch (chain.chain_name) {
-  //       case 'osmosis':
-  //         return {
-  //           gasPrice: GasPrice.fromString('0.0025uosmo'),
-  //         };
-  //       case 'juno':
-  //         return {
-  //           gasPrice: GasPrice.fromString('0.0025ujuno'),
-  //         };
-  //       case 'stargaze':
-  //         return {
-  //           gasPrice: GasPrice.fromString('0.0025ustars'),
-  //         };
-  //     }
-  //   },
-  // };
   const wallets = [keplrWallet]
 
   return (
-    <ChakraProvider theme={defaultTheme}>
-      <QueryClientProvider client={queryClient}>
-        <ChainProvider
-          chains={chains}
-          assetLists={assetLists}
-          wallets={wallets}
-          // walletConnectOptions={{
-          //   signClient: {
-          //     projectId: 'a8510432ebb71e6948cfd6cde54b70f7',
-          //     relayUrl: 'wss://relay.walletconnect.org',
-          //     metadata: {
-          //       name: 'Cosmos Kit dApp',
-          //       description: 'Cosmos Kit dApp built by Create Cosmos App',
-          //       url: 'https://docs.cosmology.zone/cosmos-kit/',
-          //       icons: [],
-          //     },
-          //   },
-          // }}
-          signerOptions={{}}
-          endpointOptions={{
-            endpoints: {}
-          }}
-        >
-          {/* TODO fix type error */}
-          {/* @ts-ignore */}
-          <Component {...pageProps} />
-        </ChainProvider>
-        {/* <ReactQueryDevtools initialIsOpen={false}/> */}
-      </QueryClientProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={defaultTheme}>
+        <QueryClientProvider client={queryClient}>
+          <ChainProvider
+            chains={_chains}
+            assetLists={_assetLists}
+            wallets={wallets}
+            signerOptions={{}}
+            endpointOptions={{
+              endpoints: {}
+            }}
+          >
+            {/* @ts-ignore */}
+            <Component {...pageProps} />
+          </ChainProvider>
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
