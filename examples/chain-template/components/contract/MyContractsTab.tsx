ef8706144b607e1da1fae070a6d2dc43992fc21a
luca <uniqueye608@gmail.com>
2024-09-14 22:02:26 +0800

    feat: add create new contract button

--- examples/chain-template/components/contract/MyContractsTab.tsx
+++ examples/chain-template/components/contract/MyContractsTab.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
-import { useChain } from '@cosmos-kit/react';
-import { Box, Icon, Spinner, Text } from '@interchain-ui/react';
+import { Box } from '@interchain-ui/react';
 
-import { useCopyToClipboard, useMyContracts } from '@/hooks';
-import { Button, Table } from '../common';
-import { shortenAddress } from '@/utils';
-import { TabLabel } from '@/pages/contract';
-import { EmptyState } from './EmptyState';
-import { useChainStore } from '@/contexts';
+import { Button } from '../common';
+import { PopoverSelect } from './PopoverSelect';
+import { MyContractsTable } from './MyContractsTable';
+
+const ContentViews = {
+  MY_CONTRACTS: 'my_contracts',
+  CREATE_FROM_UPLOAD: 'create_from_upload',
+  CREATE_FROM_CODE_ID: 'create_from_code_id',
+} as const;
+
+type ContentView = (typeof ContentViews)[keyof typeof ContentViews];
+
+const createContractOptions = [
+  { label: 'From Upload', value: ContentViews.CREATE_FROM_UPLOAD },
+  { label: 'From Code ID', value: ContentViews.CREATE_FROM_CODE_ID },
+];
 
 type MyContractsTabProps = {
   show: boolean;
@@ -15,114 +24,29 @@ type MyContractsTabProps = {
 };
 
 export const MyContractsTab = ({ show, switchTab }: MyContractsTabProps) => {
-  const { selectedChain } = useChainStore();
-  const { address } = useChain(selectedChain);
-  const { data: myContracts = [], isLoading } = useMyContracts();
+  const [contentView, setContentView] = useState<ContentView>(
+    ContentViews.MY_CONTRACTS,
+  );
 
   return (
     <Box display={show ? 'block' : 'none'} maxWidth="900px" mt="40px" mx="auto">
-      <Text color="$blackAlpha600" fontSize="24px" fontWeight="700">
-        My contracts
-      </Text>
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="300px"
-      >
-        {!address ? (
-          <EmptyState text="Connect wallet to see your previously instantiated contracts." />
-        ) : isLoading ? (
-          <Spinner size="$6xl" color="$blackAlpha600" />
-        ) : myContracts.length === 0 ? (
-          <EmptyState text="No contracts found" />
-        ) : (
-          <Box width="$full" alignSelf="start" overflowX="auto">
-            <Table minWidth="650px">
-              <Table.Header>
-                <Table.Row height="$fit">
-                  <Table.HeaderCell width="15%">
-                    Contract Address
-                  </Table.HeaderCell>
-                  <Table.HeaderCell width="20%">Contract Name</Table.HeaderCell>
-                  <Table.HeaderCell width="10%">Code ID</Table.HeaderCell>
-                  <Table.HeaderCell width="6%" />
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {myContracts.map(({ address, contractInfo }) => (
-                  <Table.Row key={address}>
-                    <Table.Cell>
-                      <CopyText
-                        displayValue={shortenAddress(address)}
-                        copyValue={address}
-                      />
-                    </Table.Cell>
-                    <Table.Cell>{contractInfo?.label}</Table.Cell>
-                    <Table.Cell color="$blackAlpha500" fontWeight="500">
-                      {Number(contractInfo?.codeId)}
-                    </Table.Cell>
-                    <Table.Cell>
-                      <Box display="flex" gap="10px">
-                        <Button
-                          size="sm"
-                          onClick={() => switchTab(address, TabLabel.Query)}
-                        >
-                          Query
-                        </Button>
-                        <Button
-                          size="sm"
-                          onClick={() => switchTab(address, TabLabel.Execute)}
-                        >
-                          Execute
-                        </Button>
-                      </Box>
-                    </Table.Cell>
-                  </Table.Row>
-                ))}
-              </Table.Body>
-            </Table>
-          </Box>
-        )}
-      </Box>
-    </Box>
-  );
-};
-
-const CopyText = ({
-  copyValue,
-  displayValue,
-}: {
-  displayValue: string;
-  copyValue: string;
-}) => {
-  const [isHover, setIsHover] = useState(false);
-  const { copyToClipboard, isCopied } = useCopyToClipboard();
-
-  return (
-    <Box
-      display="flex"
-      alignItems="center"
-      gap="8px"
-      width="$fit"
-      cursor="pointer"
-      attributes={{
-        onMouseEnter: () => setIsHover(true),
-        onMouseLeave: () => setIsHover(false),
-        onClick: () => copyToClipboard(copyValue),
-      }}
-    >
-      <Text color="$blackAlpha600" fontSize="14px" fontWeight="600">
-        {displayValue}
-      </Text>
-      {isHover && (
-        <Box display="flex" transform="translateY(2px)">
-          <Icon
-            name={isCopied ? 'checkLine' : 'copy'}
-            color={isCopied ? '$green600' : '$blackAlpha500'}
-            size="$md"
+      <MyContractsTable
+        title="My contracts"
+        show={contentView === ContentViews.MY_CONTRACTS}
+        switchTab={switchTab}
+        createContractTrigger={
+          <PopoverSelect
+            trigger={<Button variant="primary">Create New Contract</Button>}
+            options={createContractOptions}
+            onOptionClick={(value) => setContentView(value as ContentView)}
           />
-        </Box>
+        }
+      />
+      {contentView === ContentViews.CREATE_FROM_UPLOAD && (
+        <Box>Create from Upload</Box>
+      )}
+      {contentView === ContentViews.CREATE_FROM_CODE_ID && (
+        <Box>Create from Code ID</Box>
       )}
     </Box>
   );
