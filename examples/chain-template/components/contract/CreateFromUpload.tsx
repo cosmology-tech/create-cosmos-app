4f2c1186605c1ed9a239abfccdc75e2de39b2346
luca <uniqueye608@gmail.com>
2024-09-16 16:37:54 +0800

    feat: upload to instantiate contract flow

--- examples/chain-template/components/contract/CreateFromUpload.tsx
+++ examples/chain-template/components/contract/CreateFromUpload.tsx
@@ -1,18 +1,70 @@
+import { useState } from 'react';
 import { Box } from '@interchain-ui/react';
+
 import { UploadContract } from './UploadContract';
 import { BackButton } from './BackButton';
+import { Stepper } from '../common';
+import { InstantiateContract } from './InstantiateContract';
 
 type CreateFromUploadProps = {
   onBack: () => void;
 };
 
+enum Step {
+  Upload = 'Upload',
+  Instantiate = 'Instantiate',
+}
+
+const steps = [Step.Upload, Step.Instantiate];
+
 export const CreateFromUpload = ({ onBack }: CreateFromUploadProps) => {
+  const [activeStepName, setActiveStepName] = useState<Step>(Step.Upload);
+  const [completed, setCompleted] = useState(false);
+  const [codeId, setCodeId] = useState<string>();
+
+  const nextStep = () => {
+    const currentIndex = steps.indexOf(activeStepName);
+    if (currentIndex < steps.length - 1) {
+      setActiveStepName(steps[currentIndex + 1]);
+    } else {
+      setCompleted(true);
+    }
+  };
+
   return (
     <Box position="relative">
-      <Box position="absolute" top="0" left="0">
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        display="flex"
+        flexDirection="column"
+        gap="40px"
+      >
         <BackButton onClick={onBack} />
+        <Stepper
+          steps={steps}
+          activeStep={completed ? steps.length : steps.indexOf(activeStepName)}
+        />
       </Box>
-      <UploadContract show />
+      <UploadContract
+        show={activeStepName === Step.Upload}
+        onSuccess={(codeId) => {
+          setCodeId(codeId);
+          nextStep();
+        }}
+      />
+      <InstantiateContract
+        show={activeStepName === Step.Instantiate}
+        defaultCodeId={codeId}
+        switchTab={() => {}}
+        onSuccess={nextStep}
+        onCreateNewContract={() => {
+          setCompleted(false);
+          setActiveStepName(Step.Upload);
+        }}
+        onViewMyContracts={onBack}
+      />
     </Box>
   );
 };
--- examples/chain-template/components/contract/UploadContract.tsx
+++ examples/chain-template/components/contract/UploadContract.tsx
@@ -16,21 +16,26 @@ import { useStoreCodeTx } from '@/hooks';
 
 type UploadContractProps = {
   show: boolean;
+  onSuccess?: (codeId: string) => void;
 };
 
-export const UploadContract = ({ show }: UploadContractProps) => {
+export const UploadContract = ({ show, onSuccess }: UploadContractProps) => {
   const [wasmFile, setWasmFile] = useState<File | null>(null);
   const [addresses, setAddresses] = useState<Address[]>([{ value: '' }]);
   const [permission, setPermission] = useState<Permission>(
     AccessType.ACCESS_TYPE_EVERYBODY,
   );
-
   const [isLoading, setIsLoading] = useState(false);
-  const [codeId, setCodeId] = useState<string | null>(null);
 
   const { selectedChain } = useChainStore();
   const { address } = useChain(selectedChain);
   const { storeCodeTx } = useStoreCodeTx(selectedChain);
+
+  const resetStates = () => {
+    setWasmFile(null);
+    setAddresses([{ value: '' }]);
+    setPermission(AccessType.ACCESS_TYPE_EVERYBODY);
+  };
 
   const handleUpload = () => {
     if (!address || !wasmFile) return;
@@ -46,15 +51,10 @@ export const UploadContract = ({ show }: UploadContractProps) => {
       },
       onTxSucceed(codeId) {
         setIsLoading(false);
-        setCodeId(codeId);
+        onSuccess?.(codeId);
+        resetStates();
       },
     });
-  };
-
-  const resetStates = () => {
-    setWasmFile(null);
-    setAddresses([{ value: '' }]);
-    setPermission(AccessType.ACCESS_TYPE_EVERYBODY);
   };
 
   const isAddressesValid =
