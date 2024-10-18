8dc7c33eca0d180db961e6a11f1747ca7c716353
Eason Smith <eason@smi.th>
2024-10-18 10:00:53 +0800

    use toast of interchain-ui

--- examples/telescope/hooks/useTransactionToast.tsx
+++ examples/telescope/hooks/useTransactionToast.tsx
@@ -1,28 +1,23 @@
-import { useToast, Text, Box } from '@interchain-ui/react';
+import { Text, Box, toast } from '@interchain-ui/react';
 import { TransactionResult } from '../components/types';
 
 export const useTransactionToast = () => {
-  const toast = useToast({
-    position: 'top-right',
-    containerStyle: {
-      maxWidth: '150px',
-    },
-  });
 
   const showToast = (code: number, res?: any) => {
-    toast({
-      title: `Transaction ${code === TransactionResult.Success ? 'successful' : 'failed'
-        }`,
-      status: code === TransactionResult.Success ? 'success' : 'error',
-      duration: code === TransactionResult.Success ? 5000 : 20000,
-      isClosable: true,
-      description: (
-        <Box pr="20px">
-          <Text fontSize="sm">{res?.message}</Text>
-          <Text fontSize="sm">{res?.rawLog}</Text>
-        </Box>
-      ),
-    });
+    toast(
+      `Transaction ${code === TransactionResult.Success ? 'successful' : 'failed'}`,
+      {
+        duration: code === TransactionResult.Success ? 5000 : 20000,
+        description: (
+          // @ts-ignore
+          <Box
+            pr="20px"
+          >
+            <Text fontSize="sm">{res?.message}</Text>
+            <Text fontSize="sm">{res?.rawLog}</Text>
+          </Box>
+        ),
+      });
   };
 
   return { showToast };
