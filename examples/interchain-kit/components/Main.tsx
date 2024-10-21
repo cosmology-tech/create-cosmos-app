ac1071b37d6f23ab1db365cabd91d6c4b4c6307c
Eason Smith <eason@smi.th>
2024-10-21 17:01:24 +0800

    import SendMsg component

--- examples/interchain-kit/components/Main.tsx
+++ examples/interchain-kit/components/Main.tsx
@@ -2,13 +2,14 @@ import { Box, Text } from '@interchain-ui/react';
 import { useChain } from "@interchain-kit/react";
 import { defaultChainName } from "@/config";
 import { WalletState } from '@interchain-kit/core'
+import SendMsg from './SendMsg';
 
 export function Main() {
   const { status } = useChain(defaultChainName);
 
   return (
     status === WalletState.Connected ?
-      <Text>Sending Token Section</Text> :
+      <SendMsg /> :
       <Box textAlign="center">
         <Text color="$gray600" fontSize="$xl" attributes={{ my: '$20' }}>
           Please connect to your wallet.
