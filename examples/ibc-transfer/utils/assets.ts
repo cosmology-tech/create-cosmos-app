925a3a5f8dd68f199dc43a2ed61acb49d9101dc0
danny.kehu <hu-ke@hotmail.com>
2024-11-06 15:55:35 +0800

    removed @chain-registry/osmosis

--- examples/ibc-transfer/utils/assets.ts
+++ examples/ibc-transfer/utils/assets.ts
@@ -1,8 +1,19 @@
-import { asset_list, assets } from "@chain-registry/osmosis";
 import { Asset as OsmosisAsset } from "@chain-registry/types";
+import { asset_lists } from "@chain-registry/assets"
+import { assets } from "chain-registry"
 
-// @ts-ignore
-export const osmosisAssets: OsmosisAsset[] = [
-  ...assets.assets,
-  ...asset_list.assets,
-];
+let osmosisAssets: OsmosisAsset[] = []
+// assets from @chain-registry/assets
+const chainInfo = asset_lists.find(({ chain_name }) => chain_name === 'osmosis')
+if (Array.isArray(chainInfo?.assets)) {
+  osmosisAssets = [...chainInfo?.assets]
+}
+// assets from chain-registry
+let chainInfo2 = assets.find(({ chain_name }) => chain_name === 'osmosis')
+if (Array.isArray(chainInfo2?.assets)) {
+  osmosisAssets = [...osmosisAssets, ...chainInfo2.assets]
+}
+
+export {
+  osmosisAssets
+};
--- examples/swap-tokens/config/assets.ts
+++ examples/swap-tokens/config/assets.ts
@@ -1,9 +1,10 @@
 import { chains } from 'chain-registry';
 import { Asset } from '@chain-registry/types';
 import { CoinDenom } from '@chain-registry/utils';
-import { assets, asset_list } from '@chain-registry/osmosis';
 import { CoinGeckoId } from '@/hooks';
 import { defaultChainName } from '@/config';
+import { asset_lists } from "@chain-registry/assets"
+import { assets } from "chain-registry"
 
 export type Assets = Asset[] & {
   WithCoinGeckoId: Asset[];
@@ -12,9 +13,20 @@ export type Assets = Asset[] & {
   CoinGeckoIdToAsset: Record<CoinGeckoId, Asset>;
 }
 
+let osmosisAssets: Asset[] = []
+// assets from @chain-registry/assets
+const chainInfo = asset_lists.find(({ chain_name }) => chain_name === 'osmosis')
+if (Array.isArray(chainInfo?.assets)) {
+  osmosisAssets = [...chainInfo?.assets]
+}
+// assets from chain-registry
+let chainInfo2 = assets.find(({ chain_name }) => chain_name === 'osmosis')
+if (Array.isArray(chainInfo2?.assets)) {
+  osmosisAssets = [...osmosisAssets, ...chainInfo2.assets]
+}
+
 export const OsmosisAssets = [
-  ...assets.assets,
-  ...asset_list.assets
+  ...osmosisAssets
 ].filter(({ type_asset }) => type_asset !== 'ics20') as Assets;
 
 OsmosisAssets.WithCoinGeckoId = OsmosisAssets
