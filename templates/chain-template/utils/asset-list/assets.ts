eccef029c85b7a8a3b07c3ea98418fb455252aad
danny.kehu <hu-ke@hotmail.com>
2024-11-07 17:41:53 +0800

    removed @chain-registry/osmosis for examples/telescope and templates/chain-template

--- templates/chain-template/utils/asset-list/assets.ts
+++ templates/chain-template/utils/asset-list/assets.ts
@@ -1,8 +1,18 @@
-import { asset_list, assets } from "@chain-registry/osmosis";
 import { Asset as OsmosisAsset } from "@chain-registry/types";
+import { asset_lists } from "@chain-registry/assets"
+import { assets } from "chain-registry"
+
+let osmoAssets: OsmosisAsset[] = []
+
+const chainInfo = asset_lists.find(({ chain_name }) => chain_name === 'osmosis')
+if (Array.isArray(chainInfo?.assets)) {
+  osmoAssets = [...chainInfo?.assets]
+}
+
+let chainInfo2 = assets.find(({ chain_name }) => chain_name === 'osmosis')
+if (Array.isArray(chainInfo2?.assets)) {
+  osmoAssets = [...osmoAssets, ...chainInfo2.assets]
+}
 
 // @ts-ignore
-export const osmosisAssets: OsmosisAsset[] = [
-  ...assets.assets,
-  ...asset_list.assets,
-];
+export const osmosisAssets = osmoAssets
