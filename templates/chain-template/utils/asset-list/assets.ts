f89c6306258d532a8a8daaa37801e955b47973d9
luca <uniqueye608@gmail.com>
2024-11-07 18:28:37 +0800

    fix: remove comments

--- templates/chain-template/utils/asset-list/assets.ts
+++ templates/chain-template/utils/asset-list/assets.ts
@@ -1,18 +1,19 @@
-import { Asset as OsmosisAsset } from "@chain-registry/types";
-import { asset_lists } from "@chain-registry/assets"
-import { assets } from "chain-registry"
+import { Asset as OsmosisAsset } from '@chain-registry/types';
+import { asset_lists } from '@chain-registry/assets';
+import { assets } from 'chain-registry';
 
-let osmoAssets: OsmosisAsset[] = []
+let osmoAssets: OsmosisAsset[] = [];
 
-const chainInfo = asset_lists.find(({ chain_name }) => chain_name === 'osmosis')
+const chainInfo = asset_lists.find(
+  ({ chain_name }) => chain_name === 'osmosis'
+);
 if (Array.isArray(chainInfo?.assets)) {
-  osmoAssets = [...chainInfo?.assets]
+  osmoAssets = [...chainInfo?.assets];
 }
 
-let chainInfo2 = assets.find(({ chain_name }) => chain_name === 'osmosis')
+let chainInfo2 = assets.find(({ chain_name }) => chain_name === 'osmosis');
 if (Array.isArray(chainInfo2?.assets)) {
-  osmoAssets = [...osmoAssets, ...chainInfo2.assets]
+  osmoAssets = [...osmoAssets, ...chainInfo2.assets];
 }
 
-// @ts-ignore
-export const osmosisAssets = osmoAssets
+export const osmosisAssets = osmoAssets;
