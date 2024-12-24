318386c453205f8a5b1301f3ba3aa879d1d6f47a
Eason Smith <eason@smi.th>
2024-12-24 23:15:51 +0800

    useGetBalance from interchain-react

--- examples/injective/hooks/useBalance.ts
+++ examples/injective/hooks/useBalance.ts
@@ -1,9 +1,8 @@
 import { defaultRpcEndpoint as rpcEndpoint } from '@/config';
+import { defaultContext } from '@tanstack/react-query'
 import BigNumber from "bignumber.js";
 import { defaultAssetList } from "@/config";
-
-import { createGetBalance } from 'interchainjs/cosmos/bank/v1beta1/query.rpc.func'
-import { useEffect, useState, useCallback } from 'react';
+import { useGetBalance } from 'interchain-react/cosmos/bank/v1beta1/query.rpc.func'
 
 export default function useBalance({
   address,
@@ -12,34 +11,39 @@ export default function useBalance({
 }) {
 
   const coin = defaultAssetList?.assets[0];
+
   const denom = coin!.base!
+
   const COIN_DISPLAY_EXPONENT = coin!.denomUnits.find(
     (unit) => unit.denom === coin!.display
   )?.exponent as number;
 
-  const [balance, setBalance] = useState<BigNumber | null>(null);
-  const [isFetchingBalance, setIsFetchingBalance] = useState<boolean>(false);
-
-  const fetchBalance = useCallback(() => {
-    setIsFetchingBalance(true);
-    const getBalance = createGetBalance(rpcEndpoint);
-    getBalance({
+  const {
+    data: balance,
+    isSuccess: isBalanceLoaded,
+    isLoading: isFetchingBalance,
+    refetch: refetchBalance,
+  } = useGetBalance({
+    request: {
       address,
       denom,
-    }).then(({ balance }) => {
-      setBalance(new BigNumber(balance?.amount ?? 0).multipliedBy(10 ** -COIN_DISPLAY_EXPONENT))
-    }).finally(() => {
-      setIsFetchingBalance(false);
-    });
-  }, [address, denom, COIN_DISPLAY_EXPONENT]);
-
-  useEffect(() => {
-    fetchBalance();
-  }, [fetchBalance]);
+    },
+    options: {
+      context: defaultContext,
+      enabled: !!address,
+      select: ({ balance }) =>
+        new BigNumber(balance?.amount ?? 0).multipliedBy(
+          10 ** -COIN_DISPLAY_EXPONENT
+        ),
+    },
+    // with the new version, simply pass the endpoint to the clientResolver
+    clientResolver: rpcEndpoint,
+  });
 
   return {
     balance,
+    isBalanceLoaded,
     isFetchingBalance,
-    refetchBalance: fetchBalance
+    refetchBalance,
   }
 }
