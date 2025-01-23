e4374b32d9eb0a0489a82c139a5d9331b6fbb987
Eason Smith <eason@smi.th>
2025-01-24 04:34:39 +0800

    remove ibc-transfer and asset-list add ibc-asset-list fix bug that shows topTokens is undefined

--- examples/ibc-transfer/hooks/queries/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './usePrices';
-export * from './useTopTokens';
-export * from './useAssets';
-export * from './useTotalAssets';
-export * from './useBalance';
-export * from './useQueryHooks';
